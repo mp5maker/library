596673b233821dd09f36929ca5ce85bc0a6c4c87
photonkhan <khan@interconnectionbd.com>
2019-04-12 12:40:59 +0600

    Added File Upload (Incomplete)

--- js/nodejs/FileUpload.js
+++ js/nodejs/FileUpload.js
@@ -1,2 +1,36 @@
 var formidable = require('formidable');
+var http = require('http');
+var fs = require('fs');
 
+http.createServer((request, response) => {
+    var routes = {
+        form: {
+            location: '/',
+            file: "html/form.html"
+        },
+        upload: {
+            location: '/upload',
+        }
+    }
+    if (request.url) {
+        if (request.url == routes.form.location) {
+            fs.readFile(routes.form.file, function(error, data) {
+                response.writeHead(200, { "Content-Type" : "text/html" });
+                response.write(data);
+                response.end();
+            })
+        }
+        if (request.url == routes.upload.location) {
+            var form = new formidable.IncomingForm();
+            form.parse(request, (error, fields, files) => {
+                var oldpath = files.filetoupload.path;
+                var newpath = "html/" + files.filetoupload.name;
+                fs.rename(oldpath, newpath, (error) => {
+                    if (error) throw error;
+                })
+            })
+            response.writeHead(302, { 'Location': '/' });
+            response.end();
+        }
+    }
+}).listen(8081);
