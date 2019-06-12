0eaabd78d320bf6909a1485c730ef25ede593dd4
photonkhan <khan@interconnectionbd.com>
2019-06-12 18:01:33 +0600

    Added React Context

---
+++ js/typescript-react-settings/src/contexts/ThemeContext.tsx
@@ -0,0 +1,38 @@
+import * as React from 'react';
+
+export const ThemeContext = React.createContext({
+    isLightTheme: false,
+    light: { syntax: "#555", ui: "#ddd", bg: "#eee" },
+    dark: { syntax: "#ddd", ui: "#333", bg: "#555" }
+});
+
+interface ThemeContextProviderPropsInterface {
+    children: React.ReactNode
+}
+interface ThemeContextProviderStateInterface {
+    isLightTheme: boolean,
+    light: any,
+    dark: any
+}
+
+
+class ThemeContextProvider extends React.Component<ThemeContextProviderPropsInterface, ThemeContextProviderStateInterface>{
+    constructor(props: ThemeContextProviderPropsInterface) {
+        super(props)
+        this.state = {
+            isLightTheme: true,
+            light: { syntax: "#555", ui: "#ddd", bg: "#eee"},
+            dark: { syntax: "#ddd", ui: "#333", bg: "#555"}
+        }
+    }
+
+    render() {
+        return (
+            <ThemeContext.Provider value={{...this.state}}>
+                { this.props.children }
+            </ThemeContext.Provider>
+        )
+    }
+}
+
+export default ThemeContextProvider;
--- js/typescript-react-settings/src/pages/Home.tsx
+++ js/typescript-react-settings/src/pages/Home.tsx
@@ -1,5 +1,7 @@
 import * as React from 'react';
 
+import ThemeContextProvider from "../contexts/ThemeContext";
+
 import { useGet } from '../http/Get'
 
 interface homeProps {}
@@ -8,67 +10,67 @@ export const Home: React.SFC<homeProps> = () => {
     const { loading, error, data } = useGet("https://jsonplaceholder.typicode.com/posts", [])
 
     return (
-        <React.Fragment>
-            <div>
-                <h4>
-                    Home
-                </h4>
-            </div>
-            <div className="common-header my-3">
+        <ThemeContextProvider>
+            <React.Fragment>
                 <div>
-                    On the left
+                    <h4>
+                        Home
+                    </h4>
                 </div>
-                <div>
-                    On the right
-                </div>
-            </div>
-            <div>
-                <h5>Recent Comments</h5>
-                {
-                    loading ?
-                        <span>
-                            Loading...
-                        </span>
-                    : <React.Fragment></React.Fragment>
-
-                }
-                {
-                    error ?
-                        <span>
-                            Something seems to be wrong!
-                        </span>
-                    : <React.Fragment></React.Fragment>
-
-                }
-                {
-                    data ?
+                <div className="common-header my-3">
                     <div>
-                        {
-                            data.map((comment) => {
-                                return (
-                                    <div
-                                        key={comment.id}
-                                        className="card d-inline-block mr-3 my-3 w-25 box-shadow-hover">
-                                        <div className="card-body">
-                                            <div className="card-title">
-                                                <strong>
-                                                    { comment.title }
-                                                </strong>
-                                            </div>
-                                            <div className="card-text">
-                                                { comment.body }
+                        On the left
+                    </div>
+                    <div>
+                        On the right
+                    </div>
+                </div>
+                <div>
+                    <h5>Recent Comments</h5>
+                    {
+                        loading ?
+                            <span>
+                                Loading...
+                            </span>
+                        : <React.Fragment></React.Fragment>
+                    }
+                    {
+                        error ?
+                            <span>
+                                Something seems to be wrong!
+                            </span>
+                        : <React.Fragment></React.Fragment>
+                    }
+                    {
+                        data ?
+                        <div>
+                            {
+                                data.map((comment) => {
+                                    return (
+                                        <div
+                                            key={comment.id}
+                                            className="card d-inline-block mr-3 my-3 w-25 box-shadow-hover">
+                                            <div className="card-body">
+                                                <div className="card-title">
+                                                    <strong>
+                                                        { comment.title }
+                                                    </strong>
+                                                </div>
+                                                <div className="card-text">
+                                                    { comment.body }
+                                                </div>
                                             </div>
                                         </div>
-                                    </div>
-                                )
-                            })
-                        }
-                    </div>
-                    : <span>
-                        There is no data to show!
-                    </span>
-                }
-            </div>
-        </React.Fragment>
+                                    )
+                                })
+                            }
+                        </div>
+                        : <span>
+                            There is no data to show!
+                        </span>
+                    }
+                </div>
+            </React.Fragment>
+        </ThemeContextProvider>
     )
 }
