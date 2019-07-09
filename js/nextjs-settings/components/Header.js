b96db1d824b81321b2b73967013b78911a7f9462
Shabuktagin Photon Khan <khan.photon@gmail.com>
2019-07-10 00:34:29 +0600

    Added active classes in nextjs

--- js/nextjs-settings/components/Header.js
+++ js/nextjs-settings/components/Header.js
@@ -1,39 +1,74 @@
-import React, { Fragment } from 'react'
+import React, { Fragment, Component } from 'react'
+
+import Router from "next/router"
 
 import Link from "next/link"
 
+const Routes = {
+    HOME: "/",
+    ABOUT: "/about",
+    BATMAN_TV_SHOWS: "/batman-tv-shows"
+}
+
 /* Link needs to have a tag or button */
-const Header = () => (
-    <Fragment>
-        <nav className="navbar navbar-expand-lg navbar-dark bg-dark text-white">
-            <a className="navbar-brand">
-                Next JS
-            </a>
-            <ul className="navbar-nav">
-                <li className='nav-item'>
-                    <Link href="/">
-                        <a title="Home" className="nav-link">
-                            Home
-                        </a>
-                    </Link>
-                </li>
-                <li className='nav-item'>
-                    <Link href="/about">
-                        <a title="About" className="nav-link">
-                            About
-                        </a>
-                    </Link>
-                </li>
-                <li className='nav-item'>
-                    <Link href="/batman-tv-shows">
-                        <a title="Batman TV Shows" className="nav-link">
-                            Batman TV Shows
-                        </a>
-                    </Link>
-                </li>
-            </ul>
-        </nav>
-    </Fragment>
-)
+class Header extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            pathname: {
+                home: false,
+                about: false,
+                batman: false
+            }
+        }
+    }
+
+    componentDidMount() {
+        const pathname = Router.router ? Router.router.pathname ? Router.router.pathname : "" : "";
+        this.setState({
+            pathname: {
+                home: Routes.HOME == pathname ? 'active' : '',
+                about: Routes.ABOUT == pathname ? 'active' : '',
+                batman: Routes.BATMAN_TV_SHOWS == pathname ? 'active' : ''
+            }
+        })
+    }
+
+    render() {
+        const { home, about, batman } = this.state.pathname
+        return (
+            <Fragment>
+                <nav className="navbar navbar-expand-lg navbar-dark bg-dark text-white">
+                    <a className="navbar-brand">
+                        Next JS
+                    </a>
+                    <ul className="navbar-nav">
+                        <li className='nav-item'>
+                            <Link href={Routes.HOME}>
+                                <a title="Home" className={`nav-link ${home}`}>
+                                    Home
+                                </a>
+                            </Link>
+                        </li>
+                        <li className='nav-item'>
+                            <Link href={Routes.ABOUT}>
+                                <a title="About" className={`nav-link ${about}`}>
+                                    About
+                                </a>
+                            </Link>
+                        </li>
+                        <li className='nav-item'>
+                            <Link href={Routes.BATMAN_TV_SHOWS}>
+                                <a title="Batman TV Shows" className={`nav-link ${batman}`}>
+                                    Batman TV Shows
+                                </a>
+                            </Link>
+                        </li>
+                    </ul>
+                </nav>
+            </Fragment>
+        )
+    }
+}
 
 export default Header
