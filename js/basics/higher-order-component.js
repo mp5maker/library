5bfb48cb5075230f568acd558a2dda17aa6803ef
Shabuktagin Photon Khan <khan.photon@gmail.com>
2019-08-01 00:54:40 +0600

    Currying technique

--- js/basics/higher-order-component.js
+++ js/basics/higher-order-component.js
@@ -9,3 +9,16 @@ console.log(multiplyByTwo);
 console.log(multiplyByTwo(4))
 console.log(multiply(2)(4))
 
+const multipleSecondWay = (x) => (y) => x * y;
+console.log(multipleSecondWay(2)(4))
+
+
+function multiplyWithAddtion(x) {
+    let z = x + 2;
+    return function(y) {
+        return (x * y) + z;
+    }
+}
+
+// (3 * 2) + (3 + 2) = 6 + 5 = 11
+console.log(multiplyWithAddtion(3)(2))
