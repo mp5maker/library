d443f4173d9fdb31d1791651fd8c080e7dcea2f0
photonkhan <khan@interconnectionbd.com>
2019-08-05 19:32:40 +0600

    Added a canvas tutorial

---
+++ js/canvas/basics/main.js
@@ -0,0 +1,56 @@
+window.onload = init;
+
+function init() {
+    createLine();
+    createSemiCircle();
+    createSemiCircleUsingArcTo();
+}
+
+/**
+ * Created a simple line
+ */
+function createLine() {
+    let canvas = document.getElementById('line');
+    let context = canvas.getContext('2d');
+    context.lineWidth = 5;
+    context.strokeStyle = 'black';
+    // 5px on the left | 5px on the right
+    context.lineCap = "round" // butt, square [round and square increases width by 10px]
+    context.moveTo(50, 50); // Starting Point
+    context.lineTo(50, 150); // Ending Point
+    context.stroke();
+}
+
+function createSemiCircle() {
+    let canvas = document.getElementById('semi-circle');
+    let context = canvas.getContext('2d');
+    context.lineWidth = 5;
+    context.strokeStyle = 'black';
+    // x, y, radius, starting angle, ending angle, counterclockwise / clockwise
+    context.arc(40, canvas.height / 2, 40, 1.1 * Math.PI, 1.9 * Math.PI, false)
+    context.stroke();
+}
+
+function createSemiCircleUsingArcTo() {
+    let canvas = document.getElementById('arc-to');
+    let context = canvas.getContext('2d');
+    context.lineWidth = 5;
+    context.strokeStyle = "black";
+    context.lineCap = "round";
+    context.moveTo(0, 50);
+    // control x1, control y1, x2, y2, 50
+    context.arcTo(75, 50, 75, 80, 50);
+    context.stroke();
+
+}
+
+function quadraticCurve() {
+    let canvas = document.getElementById('quadratic-curve');
+    let context = canvas.getContext('2d');
+    context.lineWidth = 5;
+    context.strokeStyle = "black";
+    context.lineCap = "round";
+    context.moveTo(50, 50);
+    // control x, control y, x2, y2
+    context.quadraticCurveTo(50, 100, 50, 150);
+}
