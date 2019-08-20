8947e89d494cc0ca5cf14177e587850c07f10723
Shabuktagin Photon Khan <khan.photon@gmail.com>
2019-08-20 21:56:29 +0600

    Signals basics

--- python/django-app/core/apps.py
+++ python/django-app/core/apps.py
@@ -1,5 +1,10 @@
 from django.apps import AppConfig
 
-
 class CoreConfig(AppConfig):
     name = 'core'
+    verbose_name = "Core"
+
+    def ready(self):
+        from .signals import (
+            create_profile_handler
+        )
