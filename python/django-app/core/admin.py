96f0edcb3d56877f3c4e0a5e08a49e3c21dd0bb1
Shabuktagin Photon Khan <khan.photon@gmail.com>
2019-08-20 21:47:43 +0600

    Added Users

--- python/django-app/core/admin.py
+++ python/django-app/core/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import (
+    CustomUser,
+    Profile
+)
+
+admin.site.register(CustomUser)
+admin.site.register(Profile)
--- python/django-app/core/models.py
+++ python/django-app/core/models.py
@@ -1,6 +1,15 @@
 from django.db import models
 
+from django.conf import settings
+
 from django.contrib.auth.models import AbstractUser
 
 class CustomUser(AbstractUser):
     pass
+
+class Profile(models.Model):
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL,
+        primary_key=True,
+        on_delete=models.CASCADE
+    )
