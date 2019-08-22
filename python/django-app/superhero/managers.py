c2f4e67ecfa9742e48d82e6caebff538965321fb
Shabuktagin Photon Khan <khan.photon@gmail.com>
2019-08-23 00:17:19 +0600

    Added custom manager and querysets

--- python/django-app/superhero/managers.py
+++ python/django-app/superhero/managers.py
@@ -1,24 +1,24 @@
 from django.db import models
 
-from common.enums import (
-    PUBLISHED,
-    ON_HOLD,
-    IN_REVIEW,
-    DRAFT
+from .querysets import (
+    OriginQuerySet
 )
 
 class OriginManager(models.Manager):
-    def publshed(self):
-        return self.filter(status=PUBLISHED)
+    def get_queryset(self):
+        return OriginQuerySet(self.model, using=self._db)
+
+    def published(self):
+        return self.get_queryset().published()
 
     def on_hold(self):
-        return self.filter(status=ON_HOLD)
+        return self.get_queryset().on_hold()
 
     def in_review(self):
-        return self.filter(status=IN_REVIEW)
+        return self.get_queryset().in_review()
 
     def draft(self):
-        return self.filter(status=DRAFT)
+        return self.get_queryset().draft()
 
     def status_count(self, keyword):
-        return self.filter(status=keyword).count()
+        return   return self.get_queryset().status_count(keyword=keyword)
