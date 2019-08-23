c11cf5d6157daa9bd2579ec3cbf5c5b17df2eee6
Shabuktagin Photon Khan <khan.photon@gmail.com>
2019-08-23 10:42:14 +0600

    Understood the concept between self.request.query_params, self.request.data, self.kwargs.get

--- python/django-app/superhero/urls.py
+++ python/django-app/superhero/urls.py
@@ -4,12 +4,18 @@ app_name = 'superhero'
 
 from .views import (
     OriginList,
+    OriginCreate,
+    OriginUpdate,
+    OriginDetail,
     LocationList,
     SightingList
 )
 
 urlpatterns = [
-    path('origin/', OriginList.as_view(), name='origin'),
-    path('location/', LocationList.as_view(), name='location'),
-    path('sighting/', SightingList.as_view(), name='sighting'),
+    path('origin/', OriginList.as_view(), name='origin-list'),
+    path('origin/create', OriginCreate.as_view(), name='origin-create'),
+    path('origin/update/<int:id>', OriginUpdate.as_view(), name='origin-update'),
+    path('origin/<int:id>', OriginDetail.as_view(), name='origin-detail'),
+    path('location/', LocationList.as_view(), name='location-list'),
+    path('sighting/', SightingList.as_view(), name='sighting-list'),
 ]
--- python/django-app/superhero/views.py
+++ python/django-app/superhero/views.py
@@ -1,3 +1,5 @@
+from django.shortcuts import get_object_or_404
+
 from rest_framework.generics  import (
     ListAPIView,
     ListCreateAPIView,
@@ -7,6 +9,9 @@ from rest_framework.generics  import (
     UpdateAPIView,
     RetrieveUpdateDestroyAPIView,
 )
+
+from rest_framework.response import Response
+from rest_framework import status
 
 from .models import (
     Origin,
@@ -24,6 +29,44 @@ from .serializers import (
 class OriginList(ListAPIView):
     queryset = Origin.objects.all()
     serializer_class = OriginSerializer
+
+    def get_queryset(self, *args, **kwargs):
+        sort = self.request.query_params.get('sort', None)
+        origin = Origin.objects.order_by('-origin') if sort == 'desc' else Origin.objects.order_by('origin')
+        return origin
+
+class OriginDetail(RetrieveAPIView):
+    queryset = Origin.objects.all()
+    serializer_class = OriginSerializer
+    lookup_field = 'id'
+
+    def get_queryset(self):
+        return Origin.objects.filter(id=self.kwargs.get('id'))
+
+class OriginCreate(CreateAPIView):
+    queryset = Origin.objects.all()
+    serializer_class = OriginSerializer
+
+    def post(self, request):
+        origin = OriginSerializer(data=self.request.data)
+        if origin.is_valid():
+            origin.save()
+            return Response(origin.data, status=status.HTTP_201_CREATED)
+        return Response(origin.error, status=status.HTTP_400_BAD_REQUEST)
+
+class OriginUpdate(UpdateAPIView):
+    queryset = Origin.objects.all()
+    serializer_class = OriginSerializer
+    lookup_field = 'id'
+
+    def patch(self, request):
+        instance = self.get_object()
+        origin = OriginSerializer(instance, data=request.data, partial=True)
+        if origin.is_valid():
+            origin.save()
+            return Response(origin.data, status=status.HTTP_200_OK)
+        return Response(origin.error, status=status.HTTP_400_BAD_REQUEST)
+
 
 class LocationList(ListAPIView):
     queryset = Location.objects.all()
