389856b962e695953f7c656cdc63b744cb1b7239
Shabuktagin Photon Khan <khan.photon@gmail.com>
2019-11-03 11:38:42 +0600

    Added getUserMedia api

--- html/markups/index.js
+++ html/markups/index.js
@@ -1,7 +1,25 @@
 (function() {
     window.onload = init;
 
-    console.log(document.querySelector(`#video-chapters`).textTracks)
     function init() {
+        console.log(document.querySelector(`#video-chapters`).textTracks)
+        let webcamStream;
+        let video;
+
+        window.startWebcam = () => {
+            navigator.mediaDevices.getUserMedia({
+                audio: true,
+                video: true
+            }).then((stream) => {
+                video = document.querySelector('#webcam');
+                video.srcObject = stream;
+                video.play();
+            }).catch((error) => console.log(error))
+        }
+
+        window.stopWebcam = () => {
+            webcamStream.getTracks()[0].stop();
+            webcamStream.getTracks()[1].stop();
+        }
     }
 })()
