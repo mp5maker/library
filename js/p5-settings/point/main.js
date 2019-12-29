faa9b364795f0f437c0f4190e55ae701f1c4397c
Shabuktagin Photon Khan <khan.photon@gmail.com>
2019-12-29 22:49:14 +0600

    Added react native and p5 settings

---
+++ js/p5-settings/point/main.js
@@ -0,0 +1,17 @@
+function setup() {
+    createCanvas(500, 500);
+
+    /* For Point */
+    strokeWeight(10);
+}
+
+function draw() {
+    /* Clear Rect */
+    // background(255);
+
+    /* Point */
+    point(0, 0)
+    point(100, 0)
+    point(100, 100)
+    point(0, 100)
+}
---
+++ js/p5-settings/rect/main.js
@@ -0,0 +1,11 @@
+function setup() {
+    createCanvas(500, 500);
+}
+
+function draw() {
+    /* Clear Rect */
+    background(255);
+
+    /* Rectangle */
+    rect(0, 0, 255, 255)
+}
