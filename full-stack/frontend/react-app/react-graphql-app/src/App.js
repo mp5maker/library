7a6ca85602368001a32e055e9698f6ff21334cf2
Shabuktagin Photon Khan <khan.photon@gmail.com>
2020-02-02 13:47:10 +0600

    Added react app

---
+++ full-stack/frontend/react-app/react-graphql-app/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { BookList } from './components/BookList'
+
+function App() {
+  return (
+    <div id="main">
+      <h1>
+        Ninja's Reading List
+      </h1>
+      <BookList />
+    </div>
+  );
+}
+
+export default App;
---
+++ full-stack/frontend/react-app/react-graphql-app/src/components/BookList/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class BookList extends Component {
+    render() {
+        return (
+            <div>
+                <ul id="book-list">
+                    <li>Book name</li>
+                </ul>
+            </div>
+        )
+    }
+}
