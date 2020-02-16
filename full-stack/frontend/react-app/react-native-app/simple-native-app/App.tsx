1aa3332d2ef477343584ff0033a6a7b6730d647b
Shabuktagin Photon Khan <khan.photon@gmail.com>
2020-02-16 16:00:58 +0600

    Converted the function based component to class based component

--- full-stack/frontend/react-app/react-native-app/simple-native-app/App.tsx
+++ full-stack/frontend/react-app/react-native-app/simple-native-app/App.tsx
@@ -6,64 +6,96 @@ import {
   Image,
   TouchableHighlight,
   SafeAreaView,
-  FlatList
+  FlatList,
+  TextInput,
 } from 'react-native';
 
 import employeeList from './server/employee.json'
 
 const logo =  require('./img/icon.png');
 
-export default function App() {
-  const name = `Simple React Native App`
+interface AppPropsInterface {}
+interface AppStateInterface {
+  liked: number,
+  employee: Array<any>,
+  name: string|number
+}
 
-  const [state, setState] = React.useState({
-    liked: 0,
-    employee: employeeList
-  })
-
-  const onPress = () => {
-    const { liked } = state
-    setState({
-      ...state,
-      liked: liked + 1
-    })
+export default class App extends React.Component<AppPropsInterface, AppStateInterface> {
+  constructor(props: AppPropsInterface) {
+    super(props)
+    this.state = {
+      liked: 0,
+      employee: employeeList,
+      name: ``
+    }
+    this.onPress = this.onPress.bind(this)
+    this.onChange = this.onChange.bind(this)
+    this.addEmployee = this.addEmployee.bind(this)
   }
 
-  return (
-    <View style={styles.container}>
-      <View style={styles.innerContainer}>
-        <Text  style={styles.logo}>
-          <Image source={logo} style={styles.icon} />
-        </Text>
-        <Text style={styles.title}>
-            { name }
+  onChange({ event }: any) {
+    this.setState({
+      [event.target.name]: event.target.value
+    } as Pick<AppStateInterface, keyof AppStateInterface>)
+  }
+
+  addEmployee() {
+
+  }
+
+  onPress() {
+    const { liked } = this.state
+    this.setState({ liked: liked + 1 })
+  }
+
+  render() {
+    const name = `Simple React Native App`
+
+    return (
+      <View style={styles.container}>
+        <View style={styles.innerContainer}>
+          <Text style={styles.logo}>
+            <Image source={logo} style={styles.icon} />
           </Text>
-        <View style={styles.heart}>
-          <TouchableHighlight
-            underlayColor={'firebrick'}
-            onPress={onPress}
-            style={styles.btn}>
-            <Text style={styles.btnText}>
-              ❤ { state.liked && state.liked }
-            </Text>
-          </TouchableHighlight>
+          <Text style={styles.title}>
+            {name}
+          </Text>
+          <View style={styles.heart}>
+            <TouchableHighlight
+              underlayColor={'firebrick'}
+              onPress={this.onPress}
+              style={styles.btn}>
+              <Text style={styles.btnText}>
+                ❤ {this.state.liked && this.state.liked}
+              </Text>
+            </TouchableHighlight>
+          </View>
         </View>
-      </View>
-      <SafeAreaView>
-        <FlatList
-            data={state.employee}
-            keyExtractor={(item: any, index: number) => item.guid + index }
+        <View style={styles.form}>
+          <TextInput
+              style={styles.input}
+              placeholder={`Name`}
+              placeholderTextColor={`firebrick`}
+              onChangeText={(text) => this.onChange({ event: { target: { name: `name`, value: text }}})}
+            />
+        </View>
+        <SafeAreaView>
+          <FlatList
+            data={this.state.employee}
+            keyExtractor={(item: any, index: number) => item.guid + index}
             renderItem={({ item }: any) => (
               <View style={styles.card}>
                 <Text>
-                  { item.name }
+                  {item.name}
                 </Text>
               </View>
             )}
           />
-      </SafeAreaView>
-    </View>
-  );
+        </SafeAreaView>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
@@ -115,5 +147,18 @@ const styles = StyleSheet.create({
     height: 50,
     justifyContent: `center`,
     alignItems: `center`
+  },
+  form: {
+    marginTop: 10,
+    marginBottom: 10,
+    marginLeft: 2,
+    marginRight: 2
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: `firebrick`,
+    borderStyle: `solid`,
+    padding: 10,
+    height: 50
   }
 });
