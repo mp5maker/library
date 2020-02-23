54f08a879febb5200f6f68f06f99245f32086086
Shabuktagin Photon Khan <khan.photon@gmail.com>
2020-02-23 22:17:50 +0600

    Converted the employee form into formik and yup

--- full-stack/frontend/react-app/react-native-app/simple-native-app/forms/Employee/index.tsx
+++ full-stack/frontend/react-app/react-native-app/simple-native-app/forms/Employee/index.tsx
@@ -1,124 +1,36 @@
 import * as React from 'react'
-import { StyleSheet, View, Text, TextInput, TouchableOpacity, Alert } from 'react-native'
+import { StyleSheet, View, Text, TextInput, TouchableOpacity, Alert, Keyboard } from 'react-native'
 import get from 'lodash/get'
 import v4 from 'uuid'
+import { Formik } from 'formik'
+import * as yup from 'yup'
 
 /* Styles */
 import { Colors } from '../../styles/Colors'
 import { Fonts } from '../../styles/Fonts'
 
+export const validationSchema = yup.object({
+    name: yup.string().required(),
+    email: yup.string().email().required(),
+    age: yup.number().moreThan(18).lessThan(80).required()
+})
+
 interface EmployeeFormPropsInterface {
     onChange: (params: any) => any,
     defaultValue?: any,
     setValue?: any,
-    clear?: boolean,
     title?: string,
     submitValue?: string,
 }
 
-interface EmployeeFormStateInterface {
-    form: {
-        name: string,
-        age: string,
-        email: string
-    },
-    error: {
-        name: string,
-        age: string,
-        email: string
-    },
-}
-
-const defaultState = {
-    form: {
-        name: ``,
-        age: ``,
-        email: ``
-    },
-    error: {
-        name: ``,
-        age: ``,
-        email: ``
-    }
-}
+interface EmployeeFormStateInterface {}
 
 export class EmployeeForm extends React.Component<EmployeeFormPropsInterface, EmployeeFormStateInterface> {
     constructor(props: EmployeeFormPropsInterface) {
         super(props)
-        this.setDefault = this.setDefault.bind(this)
-        this.clear = this.clear.bind(this)
-        this.submit = this.submit.bind(this)
-        this.onChange = this.onChange.bind(this)
-        this.state = { ...defaultState }
-    }
-
-    onChange({ event }: any) {
-        this.setState({
-            form: {
-                ...this.state.form,
-                [event.target.name]: event.target.value
-            },
-            error: {
-                ...this.state.error,
-                [event.target.name]: ``
-            }
-        } as Pick<EmployeeFormStateInterface, keyof EmployeeFormStateInterface>)
-    }
-
-    submit() {
-        const name = get(this.state, 'form.name', '').trim()
-        const email = get(this.state, 'form.email', '').trim()
-        const age = get(this.state, 'form.age', '').trim()
-
-        if (name && email && age) {
-            if (this.props.onChange) this.props.onChange({
-                item: {
-                    alias: v4(),
-                    ...this.state.form
-                }
-            })
-            this.clear()
-        } else {
-            this.setState({
-                error: {
-                    ...this.state.error,
-                    ...(name ? {} : { name: `Field is required` }),
-                    ...(email ? {} : { email: `Field is required` }),
-                    ...(age ? {} : { age: `Field is required` }),
-                }
-            })
-            Alert.alert('OOPS!', 'Something seems to be wrong', [
-                {
-                    text: `Understood`,
-                    onPress: () => console.log(`Alert Closed`)
-                }
-            ])
-        }
-
-    }
-
-    clear() {
-        this.setState({ ...defaultState })
-    }
-
-    setDefault(form: any) {
-        this.setState({ form })
-    }
-
-    componentDidUpdate(prevProps: any) {
-        if (this.props.clear !== prevProps.clear) this.clear()
-        if (this.props.setValue !== prevProps.setValue) this.setDefault(this.props.setValue)
-    }
-
-    componentDidMount() {
-        if (this.props.defaultValue) this.setDefault(this.props.defaultValue)
     }
 
     render() {
-        const {
-            error
-        } = this.state
-
         const {
             title,
             submitValue
@@ -126,83 +38,129 @@ export class EmployeeForm extends React.Component<EmployeeFormPropsInterface, Em
 
         return (
             <View style={styles.container}>
-                <View>
-                    <Text style={styles.heading}>
-                        { title }
-                    </Text>
-                </View>
-                <View>
-                    <TextInput
-                        value={this.state.form.name}
-                        style={styles.textInput}
-                        placeholderTextColor={Colors.darkGreen}
-                        placeholder={`Name`}
-                        autoCompleteType={`off`}
-                        onChangeText={(value) => this.onChange({
-                            event: {
-                                target: {
-                                    name: `name`,
-                                    value
+                <Formik
+                    validationSchema={validationSchema}
+                    initialValues={{ name: "", email: "", age: "" }}
+                    onSubmit={(values, actions) => {
+                        const name = get(values, 'name', '').trim()
+                        const email = get(values, 'email', '').trim()
+                        const age = get(values, 'age', '').trim()
+
+                        if (name && email && age) {
+                            if (this.props.onChange) {
+                                const item = {
+                                    name,
+                                    email,
+                                    age,
+                                    alias: v4()
                                 }
+                                this.props.onChange({ item })
+                                actions.resetForm()
+                                Keyboard.dismiss()
                             }
-                        })} />
-                        {
-                            error.name ?  (
-                                <Text style={styles.errorMessage}>
-                                    { error.name }
-                                </Text>
-                            ) : <React.Fragment></React.Fragment>
+                        } else {
+                            Alert.alert('OOPS!', 'Something seems to be wrong', [
+                                {
+                                    text: `Understood`,
+                                    onPress: () => console.log(`Alert Closed`)
+                                }
+                            ])
                         }
-                    <TextInput
-                        value={this.state.form.email}
-                        style={styles.textInput}
-                        placeholderTextColor={Colors.darkGreen}
-                        placeholder={`Email`}
-                        autoCompleteType={`off`}
-                        onChangeText={(value) => this.onChange({
-                            event: {
-                                target: {
-                                    name: `email`,
-                                    value
-                                }
-                            }
-                        })} />
+                    }}>
                         {
-                            error.email ? (
-                                <Text style={styles.errorMessage}>
-                                    { error.email }
-                                </Text>
-                            ) : <React.Fragment></React.Fragment>
-                        }
-                    <TextInput
-                        value={this.state.form.age}
-                        style={styles.textInput}
-                        placeholderTextColor={Colors.darkGreen}
-                        placeholder={`Age`}
-                        autoCompleteType={`off`}
-                        onChangeText={(value) => this.onChange({
-                            event: {
-                                target: {
-                                    name: `age`,
-                                    value
-                                }
+                            ({ values, handleChange, errors, handleSubmit, touched, handleBlur }) => {
+
+                                const Title = (
+                                    <View>
+                                        <Text style={styles.heading}>
+                                            {title}
+                                        </Text>
+                                    </View>
+                                )
+
+                                const Name = (
+                                    <React.Fragment>
+                                        <TextInput
+                                            value={values.name}
+                                            style={styles.textInput}
+                                            placeholderTextColor={Colors.darkGreen}
+                                            placeholder={`Name`}
+                                            autoCompleteType={`off`}
+                                            onBlur={handleBlur('name')}
+                                            onChangeText={handleChange('name')} />
+                                        {
+                                            errors.name && touched.name ? (
+                                                <Text style={styles.errorMessage}>
+                                                    {errors.name}
+                                                </Text>
+                                            ) : <React.Fragment></React.Fragment>
+                                        }
+
+                                    </React.Fragment>
+                                )
+
+                                const Email = (
+                                    <React.Fragment>
+                                        <TextInput
+                                            value={values.email}
+                                            style={styles.textInput}
+                                            placeholderTextColor={Colors.darkGreen}
+                                            placeholder={`Email`}
+                                            autoCompleteType={`off`}
+                                            onBlur={handleBlur('email')}
+                                            onChangeText={handleChange('email')} />
+                                        {
+                                            errors.email && touched.email? (
+                                                <Text style={styles.errorMessage}>
+                                                    {errors.email}
+                                                </Text>
+                                            ) : <React.Fragment></React.Fragment>
+                                        }
+                                    </React.Fragment>
+                                )
+
+                                const Age = (
+                                    <React.Fragment>
+                                        <TextInput
+                                            value={values.age}
+                                            style={styles.textInput}
+                                            placeholderTextColor={Colors.darkGreen}
+                                            placeholder={`Age`}
+                                            autoCompleteType={`off`}
+                                            keyboardType={`numeric`}
+                                            onChangeText={handleChange('age')} />
+                                        {
+                                            errors.age && touched.age ? (
+                                                <Text style={styles.errorMessage}>
+                                                    {errors.age}
+                                                </Text>
+                                            ) : <React.Fragment></React.Fragment>
+                                        }
+                                    </React.Fragment>
+                                )
+
+                                const Submit = (
+                                    <TouchableOpacity
+                                        style={styles.submitButton}
+                                        onPress={() => handleSubmit()}>
+                                        <Text style={styles.submitButtonText}>
+                                            {submitValue}
+                                        </Text>
+                                    </TouchableOpacity>
+                                )
+
+                                return (
+                                    <View>
+                                        { Title }
+                                        { Name }
+                                        { Email }
+                                        { Age }
+                                        { Submit }
+                                    </View>
+                                )
                             }
-                        })} />
-                    {
-                        error.age ? (
-                            <Text style={styles.errorMessage}>
-                                { error.age }
-                            </Text>
-                        ) : <React.Fragment></React.Fragment>
-                    }
-                    <TouchableOpacity
-                        style={styles.submitButton}
-                        onPress={this.submit}>
-                        <Text style={styles.submitButtonText}>
-                            { submitValue }
-                        </Text>
-                    </TouchableOpacity>
-                </View>
+                        }
+                </Formik>
             </View>
         )
     }
