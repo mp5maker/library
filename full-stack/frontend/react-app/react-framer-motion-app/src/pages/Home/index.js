073f170aee4737aba12834557d1285e8df658d0a
Shabuktagin Photon Khan <khan.photon@gmail.com>
2020-06-07 09:06:20 +0600

    Added initial and animate attribute using framer motion

--- full-stack/frontend/react-app/react-framer-motion-app/src/pages/Home/index.js
+++ full-stack/frontend/react-app/react-framer-motion-app/src/pages/Home/index.js
@@ -1,10 +1,24 @@
 import React, { Fragment } from 'react'
+import { motion } from 'framer-motion'
 
 export const Home = () => {
     return (
         <Fragment>
-            <div>
-                Home
+            <div className={`page-home-container`}>
+                <motion.h2
+                    initial={{
+                        y: `-100vh`,
+                        opacity: 0,
+                    }}
+                    animate={{
+                        opacity: 1,
+                        fontSize: 50,
+                        color: `#ff2994`,
+                        x: 10,
+                        y: 0,
+                    }}>
+                    Home
+                </motion.h2>
             </div>
         </Fragment>
     )
