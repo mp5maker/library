2ed5ee92a3606594fc31d06e4da4fb0f96b9f1dd
Shabuktagin Photon Khan <khan.photon@gmail.com>
2020-06-07 14:45:17 +0600

    Added a demo for whileHover

--- full-stack/frontend/react-app/react-framer-motion-app/src/pages/Home/index.js
+++ full-stack/frontend/react-app/react-framer-motion-app/src/pages/Home/index.js
@@ -1,32 +1,96 @@
 import React, { Fragment } from 'react'
+import { Link } from 'react-router-dom'
 import { motion } from 'framer-motion'
 
 export const Home = () => {
     return (
         <Fragment>
-            <div className={`page-home-container`}>
+            <motion.div
+                initial={{
+                    x: "1000vw",
+                    opacity: 0,
+                }}
+                animate={{
+                    x: 0,
+                    opacity: 1
+                }}
+                transition={{
+                    type: `spring`,
+                    delay: 0.5
+                }}
+                style={{
+                    height: `100vh`,
+                    backgroundColor: `lightgrey`
+                }}
+                className={`page-home-container`}>
                 <motion.h2
                     initial={{
                         y: `-100vh`,
                         opacity: 0,
                     }}
                     transition={{
-                        delay: 0.25,
+                        delay: 1,
                         duration: 0.25,
                         type: `spring`,
+                        // [spring, tween]
                         /* Works for spring only */
                         stiffness: 50
                     }}
                     animate={{
                         opacity: 1,
                         fontSize: 50,
-                        color: `#ff2994`,
                         x: 10,
                         y: 0,
                     }}>
                     Home
                 </motion.h2>
-            </div>
+                <Link to={`/about-us`}>
+                    <motion.button
+                        style={{
+                            marginLeft: `10px`,
+                            border: `none`,
+                            padding: `12px`,
+                            outline: `none`,
+                            borderRadius: `5px`
+                        }}
+                        transition={{
+                            type: `spring`,
+                            stiffness: 150
+                        }}
+                        whileHover={{
+                            boxShadow: `0 1px 15px 0 ligthgrey`,
+                            scale: 1.1,
+                            originX: 0,
+                            color: `white`,
+                            border: `1px solid white`
+                        }}>
+                        About Us
+                    </motion.button>
+                </Link>
+                <Link to={`/contact-us`}>
+                    <motion.button
+                        style={{
+                            marginLeft: `10px`,
+                            border: `none`,
+                            padding: `12px`,
+                            outline: `none`,
+                            borderRadius: `5px`
+                        }}
+                        transition={{
+                            type: `spring`,
+                            stiffness: 150
+                        }}
+                        whileHover={{
+                            boxShadow: `0 1px 15px 0 ligthgrey`,
+                            scale: 1.1,
+                            originX: 0,
+                            color: `white`,
+                            border: `1px solid white`
+                        }}>
+                        Contact Us
+                    </motion.button>
+                </Link>
+            </motion.div>
         </Fragment>
     )
 }
