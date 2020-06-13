396fec8edcee2d11da2d1d53277508d24f86b479
mp5maker <khan.photon@gmail.com>
2020-06-13 23:20:22 +0600

    Added key down

--- full-stack/frontend/react-app/react-socket-io-app/frontend/src/socket/index.js
+++ full-stack/frontend/react-app/react-socket-io-app/frontend/src/socket/index.js
@@ -3,19 +3,24 @@ import socketIO from 'socket.io-client'
 import get from 'lodash/get'
 
 import { SERVER } from '../constants/Routes'
+import "./Socket.css"
 
 const socket = socketIO(SERVER)
 export const Socket = () => {
     const [messages, setMessages] = useState([])
     const [enterAMessage, setEnterAMessage] = useState("")
 
+    const onKeyDown = (event) => {
+        if (event.keyCode === 13 && event.key === 'Enter') onSubmit()
+    }
+
     const onChange = (event) => {
         const msg = get(event, 'target.value', '')
         setEnterAMessage(msg)
     }
 
-    const onSubmit = (event) => {
-        event.preventDefault()
+    const onSubmit = (event = "") => {
+        event && event.preventDefault()
         /* Chat */
         socket.emit("chat", enterAMessage)
         setEnterAMessage("")
@@ -39,28 +44,31 @@ export const Socket = () => {
 
     return (
         <React.Fragment>
-            <div className={`messages`}>
-                {
-                    messages.map((message, key) => {
-                        return (
-                            <React.Fragment key={key}>
-                                <div>
-                                    { message }
-                                </div>
-                            </React.Fragment>
-                        )
-                    })
-                }
-            </div>
-            <div className={`type`}>
-                <input
-                    value={enterAMessage}
-                    onChange={onChange}
-                    name="enterAMessage"
-                    type="text" />
-                <button onClick={onSubmit}>
-                    Send
-                </button>
+            <div className="chatroom">
+                <div className={`messages`}>
+                    {
+                        messages.map((message, key) => {
+                            return (
+                                <React.Fragment key={key}>
+                                    <div>
+                                        { message }
+                                    </div>
+                                </React.Fragment>
+                            )
+                        })
+                    }
+                </div>
+                <div className={`type`}>
+                    <input
+                        onKeyDown={onKeyDown}
+                        value={enterAMessage}
+                        onChange={onChange}
+                        name="enterAMessage"
+                        type="text" />
+                    <button onClick={onSubmit}>
+                        Send
+                    </button>
+                </div>
             </div>
         </React.Fragment>
     )
