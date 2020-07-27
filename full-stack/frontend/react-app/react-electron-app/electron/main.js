88d2548d529765c9ae0381fd33075a1308d4378b
mp5maker <khan.photon@gmail.com>
2020-07-27 11:28:47 +0600

    feat: add a simple electron app with react starter

---
+++ full-stack/frontend/react-app/react-electron-app/electron/main.js
@@ -0,0 +1,33 @@
+const { app, BrowserWindow } = require('electron')
+const path = require('path')
+const url = require('url')
+
+function createWindow() {
+    const startUrl = process.env.ELECTRON_START_URL || url.format({
+        pathname: path.join(__dirname, '../index.html'),
+        protocol: 'file:',
+        slashes: true,
+    });
+
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true
+        }
+    })
+
+    win.loadURL(startUrl);
+    win.webContents.openDevTools()
+}
+
+app.whenReady().then(createWindow)
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
+})
+
+app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+})
