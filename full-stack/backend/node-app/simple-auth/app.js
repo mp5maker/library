983784b09cf894e145d72338d0e56bc84efe32e7
mp5maker <khan.photon@gmail.com>
2020-08-15 20:31:14 +0600

    fix: git guardian to remove easily accessible credentials

--- full-stack/backend/node-app/simple-auth/app.js
+++ full-stack/backend/node-app/simple-auth/app.js
@@ -1,3 +1,4 @@
+const env = require('dotenv').config().parsed
 const express = require('express')
 const mongoose = require('mongoose')
 const authRoutes = require('./routes/authRoutes')
@@ -12,7 +13,7 @@ app.use(express.json())
 app.set('view engine', 'ejs')
 
 /* Database Connection */
-const databaseURI = 'mongodb+srv://test:123@cluster0.gs6cu.mongodb.net/node_auth'
+const databaseURI = `mongodb+srv://${env.USERNAME}:${env.PASSWORD}@cluster0.gs6cu.mongodb.net/${env.DATABASE_NAME}`
 mongoose.connect(databaseURI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
     .then((result) => app.listen(3000))
     .catch((error) => console.log(error))
