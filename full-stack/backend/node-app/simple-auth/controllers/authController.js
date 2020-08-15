21796892e652ece1d0c364096a2b7fbf816c61dc
mp5maker <khan.photon@gmail.com>
2020-08-15 21:16:58 +0600

    feat: add validation in node autth app

--- full-stack/backend/node-app/simple-auth/controllers/authController.js
+++ full-stack/backend/node-app/simple-auth/controllers/authController.js
@@ -1,4 +1,20 @@
 const User = require('../models/User')
+const get = require('lodash/get')
+
+/* Handle Errors */
+const handleErrors = (error) => {
+    if (error.code === 11000) return { email: `EMAIL_ALREADY_EXIST` }
+    if (error.message.includes('user validation failed')) {
+        return Object.values(error.errors).reduce((errorObj, err) => {
+            const path = get(err, 'path', '')
+            const message = get(err, 'message', '')
+            return {
+                ...errorObj,
+                [path]: message
+            }
+        }, {})
+    }
+}
 
 module.exports.signup_get = (request, response) => {
     response.render('signup')
@@ -10,20 +26,16 @@ module.exports.login_get = (request, response) => {
 
 module.exports.signup_post = async (request, response) => {
     const { email, password } = request.body
-
     try {
         const user = await User.create({ email, password })
         response.status(201).json(user)
-    } catch (err) {
-        console.log(err)
-        response.status(400).send('error, user not created')
+    } catch (error) {
+        const errors = handleErrors(error)
+        response.status(400).json({ errors })
     }
-
-    response.send('new signup')
 }
 
 module.exports.login_post = (request, response) => {
     const { email, password } = request.body
-    console.log(email, password)
     response.send('user login')
 }
--- full-stack/backend/node-app/simple-auth/models/User.js
+++ full-stack/backend/node-app/simple-auth/models/User.js
@@ -1,16 +1,18 @@
 const mongoose = require('mongoose')
+const { isEmail } = require('validator')
 
 const schema = new mongoose.Schema({
     email: {
         type: String,
-        required: true,
+        required: [true, 'PLEASE_ENTER_AN_EMAIL'],
         unique: true,
-        lowercase: true
+        lowercase: true,
+        validate: [isEmail, 'PLEASE_ENTER_A_VALID_EMAIL']
     },
     password: {
         type: String,
-        required: true,
-        minLength: 6
+        required: [true, 'PLEASE_ENTER_AN_PASSWORD'],
+        minlength: [6, 'MINIMUM_PASSWORD_LENGTH_IS_SIX_CHARACTERS']
     }
 })
 
