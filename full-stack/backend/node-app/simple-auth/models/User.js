58da4cfa6850dac9138c9a4283d4ea1ac8d8e2bb
mp5maker <khan.photon@gmail.com>
2020-08-18 17:31:03 +0600

    feat: add auth view

--- full-stack/backend/node-app/simple-auth/models/User.js
+++ full-stack/backend/node-app/simple-auth/models/User.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose')
 const { isEmail } = require('validator')
+const bcrypt = require('bcrypt')
 
 const schema = new mongoose.Schema({
     email: {
@@ -23,8 +24,9 @@ schema.post('save', function(doc, next) {
 })
 
 /* Fire a function before doc saved to db */
-schema.pre('save', function(next) {
-    console.log('user about to be created & saved', this)
+schema.pre('save', async function(next) {
+    const salt = await bcrypt.genSalt()
+    this.password = await bcrypt.hash(this.password, salt)
     next()
 })
 
