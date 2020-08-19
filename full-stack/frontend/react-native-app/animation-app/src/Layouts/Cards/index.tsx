93d12437220dd560b44ba19b027eb7b724c94dc3
mp5maker <khan.photon@gmail.com>
2020-08-19 15:17:17 +0600

    feat: add transitions in the route

---
+++ full-stack/frontend/react-native-app/animation-app/src/Layouts/Cards/index.tsx
@@ -0,0 +1,83 @@
+import * as React from 'react'
+import { View, Text, StyleSheet } from 'react-native'
+import Animated from 'react-native-reanimated'
+import { useTransition, mix } from 'react-native-redash'
+
+const alpha = Math.PI / 6
+
+const randomColor = () => {
+    let n = (Math.random() * 0xfffff * 1000000).toString(16);
+    return '#' + n.slice(0, 6);
+};
+
+export const Cards = ({ list = [], allowRotation = false, pivotCenter = false }: any) => {
+    const transition = useTransition(allowRotation, { duration: 400 })
+
+    return (
+        <>
+            <View
+                style={[
+                    styles.container,
+                ]}>
+                {
+                    list.map((item: any, index: number) => {
+                        // const rotate = Animated.interpolate(transition, {
+                        //     inputRange: [0, 1],
+                            // outputRange: [0, (index - 1) * alpha]
+                        // })
+                        const rotate = mix(transition, 0, (index - 1) * alpha)
+                        const zIndex = 2 - index
+
+                        return (
+                            <Animated.View
+                                style={[
+                                    styles.item,
+                                    { transform: [{ rotate }] },
+                                    { ...(pivotCenter ? {
+                                        borderColor: randomColor(),
+                                        backgroundColor: randomColor(),
+                                        position: `absolute`,
+                                        top: 0,
+                                        left: 0,
+                                        zIndex
+                                    } : {} )}
+                                ]}
+                                key={index}>
+                                <Text>
+                                    { item }
+                                </Text>
+                            </Animated.View>
+                        )
+                    })
+                }
+            </View>
+        </>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        display: `flex`,
+        justifyContent: `flex-start`,
+        alignItems: `flex-start`
+    },
+    item: {
+        padding: 12,
+        width: 299,
+        height: 200,
+        borderWidth: 1,
+        borderColor: `lightgrey`,
+        shadowOffset: {
+            width: 1,
+            height: 1,
+        },
+        shadowOpacity: 0.3,
+        shadowRadius: 2,
+        elevation: 2,
+        marginBottom: 12,
+        borderRadius: 5,
+        display: `flex`,
+        justifyContent: `center`,
+        alignItems: `center`
+    }
+})
--- full-stack/frontend/react-native-app/animation-app/src/Screens/Home/Transition/index.tsx
+++ full-stack/frontend/react-native-app/animation-app/src/Screens/Home/Transition/index.tsx
@@ -1,9 +1,28 @@
 import * as React from 'react'
-import { View, StyleSheet } from 'react-native'
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native'
+import { useTranslation } from 'react-i18next'
+
+import { Cards } from 'Native/Layouts/Cards'
 
 export const TransitionScreen = () => {
+    const { t } = useTranslation()
+    const [toggle, setToggle] = React.useState(false)
+
     return (
         <View style={styles.mainContainer}>
+            <TouchableOpacity
+                style={styles.button}
+                onPress={() => setToggle(!toggle)}>
+                <Text>
+                    { t(`ALLOW_ROTATION`) }
+                </Text>
+            </TouchableOpacity>
+            <View style={styles.cardSection}>
+                <Cards
+                    pivotCenter={true}
+                    allowRotation={toggle}
+                    list={['Hi', 'Hello', 'Bye']} />
+            </View>
         </View>
     )
 }
@@ -11,7 +30,17 @@ export const TransitionScreen = () => {
 export const styles = StyleSheet.create({
     mainContainer: {
         flex: 1,
-        alignItems: `center`,
-        justifyContent: `center`
+        alignItems: `flex-start`,
+        justifyContent: `flex-start`
     },
+    button: {
+        padding: 12,
+        margin: 12
+    },
+    cardSection: {
+        position: `relative`,
+        margin: 12,
+        padding: 12,
+        marginTop: 40
+    }
 });
