2f668c8e18e57e17dd15bebfafa61836a4988889
mp5maker <khan.photon@gmail.com>
2020-08-24 12:28:05 +0600

    feat: add simple query to fetch data from db

--- full-stack/backend/node-app/lireddit-server/dist/index.js
+++ full-stack/backend/node-app/lireddit-server/dist/index.js
@@ -13,10 +13,13 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const core_1 = require("@mikro-orm/core");
+const Post_1 = require("./entities/Post");
 const mikro_orm_config_1 = __importDefault(require("./mikro-orm.config"));
 const main = () => __awaiter(void 0, void 0, void 0, function* () {
     const orm = yield core_1.MikroORM.init(mikro_orm_config_1.default);
     yield orm.getMigrator().up();
+    const posts = yield orm.em.find(Post_1.Post, {});
+    console.log(posts);
 });
 main().catch((error) => console.error(error));
 //# sourceMappingURL=index.js.map
--- full-stack/backend/node-app/lireddit-server/src/index.ts
+++ full-stack/backend/node-app/lireddit-server/src/index.ts
@@ -6,10 +6,14 @@ import microConfig from "./mikro-orm.config"
 const main = async () => {
     const orm = await MikroORM.init(microConfig)
     await orm.getMigrator().up()
-    // const post = orm.em.create(Post, { title: 'my first post' })
+
+    /* Create Example */
+    // const post = orm.em.create(Post, { title: 'my second post' })
     // await orm.em.persistAndFlush(post)
-    // console.log(":::::::::::::::::::::::::::: sql 2 ::::::::::::::::::::")
-    // await orm.em.nativeInsert(Post, { title: "my second post" })
+
+    /* Find Example */
+    const posts = await orm.em.find(Post, {})
+    console.log(posts)
 }
 
 main().catch((error) => console.error(error))
