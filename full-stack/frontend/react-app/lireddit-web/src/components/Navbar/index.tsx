efcdb41bc255c0fd5a9951ccfb94b655c91f0359
mp5maker <khan.photon@gmail.com>
2020-08-25 18:05:15 +0600

    feat: add navbar

---
+++ full-stack/frontend/react-app/lireddit-web/src/components/Navbar/index.tsx
@@ -0,0 +1,55 @@
+import React from 'react'
+import { Box, Link, Flex, Button } from '@chakra-ui/core'
+import NextLink from 'next/link'
+import { useMeQuery } from '../../generated/graphql'
+
+interface NavBarProps {}
+
+export const NavBar: React.FC<NavBarProps> = ({}) => {
+    const [{ data, fetching }] = useMeQuery()
+
+    let body = null
+    if (fetching) {
+
+    } else if(!data?.me) {
+        body = (
+            <>
+                <NextLink href="/login">
+                    <Link mr={2}>
+                        Login
+                    </Link>
+                </NextLink>
+                <NextLink href="/register">
+                    <Link mr={2}>
+                        Register
+                    </Link>
+                </NextLink>
+            </>
+        )
+    } else {
+        body = (
+            <>
+                <Flex>
+                    <Box mr={2}>
+                        { data?.me?.username }
+                    </Box>
+                    <Button variant={`link`}>
+                        Logout
+                    </Button>
+                </Flex>
+            </>
+        )
+    }
+
+    return (
+        <>
+            <Flex
+                p={4}
+                bg="tomato">
+                <Box ml={"auto"}>
+                    { body }
+                </Box>
+            </Flex>
+        </>
+    )
+}
--- full-stack/frontend/react-app/lireddit-web/src/pages/index.tsx
+++ full-stack/frontend/react-app/lireddit-web/src/pages/index.tsx
@@ -1,7 +1,10 @@
+import { NavBar } from "../components/Navbar"
+
 const Index = () => (
-  <div>
+  <>
+    <NavBar />
     Hello world
-  </div>
+  </>
 )
 
 export default Index
