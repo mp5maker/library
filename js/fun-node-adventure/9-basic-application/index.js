59cf63b5486416298825ee07fa86bea552541b5e
mp5maker <khan.photon@gmail.com>
2020-08-31 19:08:54 +0600

    feat: add few basic concepts on node

---
+++ js/fun-node-adventure/9-basic-application/index.js
@@ -0,0 +1,10 @@
+const fs = require('fs')
+
+/* Current files in the directory */
+console.log(fs.readdirSync(__dirname))
+
+/* Current Async files in the directory */
+const doSomethingWithIt = (error, files) => {
+    console.log(files)
+}
+console.log(fs.readdirSync('.', doSomethingWithIt))
