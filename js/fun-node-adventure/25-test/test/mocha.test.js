026e8080d4bd505c7dad4f8680cdb1c67ed8d38e
mp5maker <khan.photon@gmail.com>
2020-09-02 21:31:10 +0600

    feat: add mocha and chai in node practice

---
+++ js/fun-node-adventure/25-test/test/mocha.test.js
@@ -0,0 +1,10 @@
+const assert = require('assert')
+
+describe('Simple Math Test', () => {
+    it('Should Return 2', () => {
+        assert.equal(1 + 1, 2)
+    })
+    it('Should Return 9', () => {
+        assert.equal(3 * 3, 9)
+    })
+})
