56ba54aac1f94b9fd7d2f7f90241967e3fecb66e
mp5maker <khan.photon@gmail.com>
2020-09-03 17:18:11 +0600

    feat: add token refresh link for the apollo client

--- full-stack/backend/node-app/jwt-auth-server/src/auth.ts
+++ full-stack/backend/node-app/jwt-auth-server/src/auth.ts
@@ -5,7 +5,7 @@ export const createAccessToken = (user: User) => {
     return sign(
         { userId: user.id },
         process.env.ACCESS_TOKEN_SECRET!,
-        { expiresIn: "15m"}
+        { expiresIn: "15s"}
     );
 };
 
--- full-stack/frontend/react-app/jwt-auth-client/src/index.tsx
+++ full-stack/frontend/react-app/jwt-auth-client/src/index.tsx
@@ -1,24 +1,80 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { App } from './App';
-import ApolloClient from 'apollo-boost'
+import { ApolloClient } from 'apollo-client';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { HttpLink } from 'apollo-link-http';
+import { onError } from 'apollo-link-error';
+import { ApolloLink, Observable } from 'apollo-link';
 import { ApolloProvider } from '@apollo/react-hooks'
-import { getAccessToken } from './accessToken';
+import { getAccessToken, setAccessToken } from './accessToken';
+import { TokenRefreshLink } from 'apollo-link-token-refresh'
+import jwtDecode from 'jwt-decode'
 
-const client = new ApolloClient({
-  uri: "http://localhost:4000/graphql",
-  credentials: 'include',
-  request: (operation) => {
-    const accessToken = getAccessToken()
-    operation.setContext({
-      ...(accessToken ? {
-        headers: {
-          authorization: accessToken ? `bearer ${accessToken}` : ''
+
+const cache = new InMemoryCache({});
+
+const requestLink = new ApolloLink((operation, forward) =>
+  new Observable(observer => {
+    let handle: any;
+    Promise.resolve(operation)
+      .then((operation) => {
+        const accessToken = getAccessToken()
+        operation.setContext({
+          ...(accessToken ? {
+            headers: {
+              authorization: accessToken ? `bearer ${accessToken}` : ''
+            }
+          } : {})
+        })
+      })
+      .then(() => {
+        handle = forward(operation).subscribe({
+          next: observer.next.bind(observer),
+          error: observer.error.bind(observer),
+          complete: observer.complete.bind(observer),
+        });
+      })
+      .catch(observer.error.bind(observer));
+
+    return () => {
+      if (handle) handle.unsubscribe();
+    };
+  })
+);
+
+const client: any = new ApolloClient({
+  link: ApolloLink.from([
+    new TokenRefreshLink({
+      accessTokenField: 'accessToken',
+      isTokenValidOrUndefined: () => {
+        const token = getAccessToken()
+        if (!token) return true
+
+        try {
+          const { exp } = jwtDecode(token)
+          if (Date.now() >= exp * 1000) return false
+          else return true
+        } catch (error) {
+          return false
         }
-      } : {})
+      },
+      fetchAccessToken: () => fetch('http://localhost:4000/refresh-token', { credentials: "include", method: "POST" }),
+      handleFetch: accessToken => setAccessToken(accessToken),
+      handleError: err => console.log(err)
+    }) as any,
+    onError(({ graphQLErrors, networkError }) => {
+      if (graphQLErrors) console.log(graphQLErrors);
+      if (networkError) console.log(networkError)
+    }),
+    requestLink,
+    new HttpLink({
+      uri: "http://localhost:4000/graphql",
+      credentials: 'include',
     })
-  }
-}) as any
+  ]),
+  cache
+});
 
 ReactDOM.render(
   <React.StrictMode>
