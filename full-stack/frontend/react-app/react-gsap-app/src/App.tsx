6b5ea7c6634a2419aa2d90db453d7ecf1645c274
mp5maker <khan.photon@gmail.com>
2020-09-20 02:03:26 +0600

    feat: add greensock in react

--- full-stack/frontend/react-app/react-gsap-app/src/App.tsx
+++ full-stack/frontend/react-app/react-gsap-app/src/App.tsx
@@ -1,25 +1,59 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import gsap from 'gsap'
+import { ScrollTrigger } from 'gsap/ScrollTrigger'
 
-function App() {
+if (gsap && ScrollTrigger) gsap.registerPlugin(ScrollTrigger)
+
+interface AppPropsInterface {}
+
+const App: React.FC<AppPropsInterface> = () => {
+  React.useEffect(() => {
+    const t1 = gsap.timeline()
+    t1.from('.acting-content-1', { xPercent: -100 })
+    t1.from('.acting-content-2', { xPercent: 100 })
+    t1.from('.acting-content-3', { yPercent: -100 })
+    t1.from('.acting-content-4', { yPercent: 100 })
+
+    ScrollTrigger.create({
+      animation: t1,
+      trigger: '.col',
+      pin: true,
+      scrub: true,
+      start: `top top`,
+      end: "+4000",
+      anticipatePin: 1,
+    })
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col">
+            <div className="actual-content"></div>
+            <div className="acting-content-1">
+              <div>
+                1
+              </div>
+            </div>
+            <div className="acting-content-2">
+              <div>
+                2
+              </div>
+            </div>
+            <div className="acting-content-3">
+              <div>
+                3
+              </div>
+            </div>
+            <div className="acting-content-4">
+              <div>
+                4
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
   );
 }
 
