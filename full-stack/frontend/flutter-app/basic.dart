1c425a2dd6c998841bc2b4e0888d7e50c5435d20
mp5maker <khan.photon@gmail.com>
2020-10-23 15:00:04 +0600

    feat: add row, columns, expanded, image, text, button, icon preliminary learning for flutter

--- full-stack/frontend/flutter-app/basic.dart
+++ full-stack/frontend/flutter-app/basic.dart
@@ -15,16 +15,98 @@ class Home extends StatelessWidget {
         centerTitle: true,
         backgroundColor: Colors.red[600],
       ),
-      body: Center(
-        child: IconButton(
-            onPressed: () {
-              print('You clicked icon me');
-            },
-            icon: Icon(
-                Icons.mail
+      body: Row(
+        children: [
+          Expanded(
+            flex: 3,
+            child: Image.asset('assets/space-2.jpg'),
+          ),
+          Expanded(
+            flex: 1,
+            child: Container(
+              padding: EdgeInsets.all(30.0),
+              color: Colors.cyan,
+              child: Text('1')
             ),
-            color: Colors.red[200]
-        )
+          ),
+          Expanded(
+            flex: 2,
+            child: Container(
+                padding: EdgeInsets.all(30.0),
+                color: Colors.pinkAccent,
+                child: Text('2')
+            ),
+          ),
+          Expanded(
+            flex: 1,
+            child: Container(
+                padding: EdgeInsets.all(30.0),
+                color: Colors.amber,
+                child: Text('3')
+            ),
+          ),
+        ]
+      ),
+      // body: Column(
+      //   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      //   crossAxisAlignment: CrossAxisAlignment.stretch,
+      //   children: [
+      //       Container(
+      //         padding: EdgeInsets.all(20.0),
+      //         color: Colors.cyan[500],
+      //         child: Text('one'),
+      //       ),
+      //       Container(
+      //         padding: EdgeInsets.all(30.0),
+      //         color: Colors.pinkAccent,
+      //         child: Text('one'),
+      //       ),
+      //       Container(
+      //         padding: EdgeInsets.all(40.0),
+      //         color: Colors.amber[500],
+      //         child: Text('one'),
+      //       ),
+      //   ],
+      // ),
+      // body: Row(
+      //   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      //   crossAxisAlignment: CrossAxisAlignment.center,
+      //   children: <Widget>[
+      //     Text('Hello World'),
+      //     FlatButton(
+      //       onPressed: () {},
+      //       color: Colors.amber,
+      //       child: Text('Click me')
+      //     ),
+      //     Container(
+      //       color: Colors.cyan,
+      //       padding: EdgeInsets.all(30.0),
+      //       child: Text('Inside container')
+      //     )
+      //   ],
+      // ),
+      // body: Padding(
+      //   padding: EdgeInsets.all(50.0),
+      //   child: Text('hello'),
+      // ),
+      // body: Container(
+      //   padding: EdgeInsets.all(20.0),
+      //   // padding: EdgeInsets.symmetric(horizontal: 30.0, vertical: 10.0),
+      //   // padding: EdgeInsets.fromLTRB(5, 20, 25, 5),
+      //   margin: EdgeInsets.all(20.0),
+      //   color: Colors.grey[400],
+      //   child: Text('hello'),
+      // ),
+      // body: Center(
+      //   child: IconButton(
+      //       onPressed: () {
+      //         print('You clicked icon me');
+      //       },
+      //       icon: Icon(
+      //           Icons.mail
+      //       ),
+      //       color: Colors.red[200]
+      //   )
         // child: RaisedButton.icon(
         //     onPressed: () {
         //       print('You clicked me');
