c200b0c3683a82b1233a0cd30b563e776591d1f7
mp5maker <khan.photon@gmail.com>
2020-11-05 00:01:01 +0600

    feat: updated the looks of the post card

--- js/pwa-app/static/js/ui.js
+++ js/pwa-app/static/js/ui.js
@@ -1,4 +1,4 @@
 document.addEventListener("DOMContentLoaded", function () {
     var sidenavs = document.querySelectorAll(".sidenav");
-    M.Sidenav.init(sidenavs, {});
+    M.Sidenav.init(sidenavs, { edge: 'right' });
 });
