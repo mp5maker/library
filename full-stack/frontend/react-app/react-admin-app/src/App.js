014ad38ec536fe05210a5460777f53a482d8964d
mp5maker <khan.photon@gmail.com>
2021-01-05 21:23:06 +0600

    feat: add simple edit guesser

--- full-stack/frontend/react-app/react-admin-app/src/App.js
+++ full-stack/frontend/react-app/react-admin-app/src/App.js
@@ -1,5 +1,8 @@
 import * as React from "react";
-// import { Admin, Resource, ListGuesser } from 'react-admin'
+import {
+  ListGuesser,
+  // EditGuesser
+} from 'react-admin'
 import { Admin, Resource } from "react-admin";
 import jsonServerProvider from "ra-data-json-server";
 import { UserList } from "./data-grid/user-list";
@@ -9,8 +12,9 @@ const dataProvider = jsonServerProvider("https://jsonplaceholder.typicode.com");
 const App = () => {
   return (
     <Admin dataProvider={dataProvider}>
-      <Resource name="posts" list={PostList} />
+      <Resource name="posts" list={PostList}/>
       <Resource name="users" list={UserList} />
+      <Resource name="todos" list={ListGuesser} />
     </Admin>
   );
 };
--- full-stack/frontend/react-app/react-admin-app/src/data-grid/post-list.js
+++ full-stack/frontend/react-app/react-admin-app/src/data-grid/post-list.js
@@ -1,15 +1,17 @@
 import * as React from "react";
-import { List, Datagrid, TextField, ReferenceField } from "react-admin";
+import { List, Datagrid, TextField, ReferenceField, EditButton } from "react-admin";
 
 export const PostList = (props) => (
   <List {...props}>
     <Datagrid rowClick="edit">
+      <TextField source="id" />
       <ReferenceField source="userId" reference="users">
         <TextField source="name" />
       </ReferenceField>
       <TextField source="id" />
       <TextField source="title" />
       <TextField source="body" />
+      <EditButton />
     </Datagrid>
   </List>
 );
