15ecb8db85df4dd91091110f3059142dea31cccf
mp5maker <khan.photon@gmail.com>
2021-01-13 23:20:48 +0600

    feat: add simple foreign key

--- full-stack/all-in-one/pern-yelp-app/server/db.sql
+++ full-stack/all-in-one/pern-yelp-app/server/db.sql
@@ -12,3 +12,11 @@ SELECT * FROM restaurants;
 
 INSERT INTO restaurants (name, location, price_range) values ('mcdonalds', 'new yorks', 3);
 INSERT INTO restaurants (name, location, price_range) values ('pizza hut', 'vegas', 2);
+
+CREATE TABLE reviews (
+  id BIGSERIAL PRIMARY KEY NOT NULL,
+  restaurant_id BIGINT NOT NULL REFERENCES restaurants(id),
+  name VARCHAR(255) NOT NULL,
+  review TEXT NOT NULL,
+  rating INT NOT NULL check(rating >= 1 and rating <= 5)
+);
