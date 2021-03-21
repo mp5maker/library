b2ad99f53cd6d3f6345b58913b8c52d1ea454676
mp5maker <khan.photon@gmail.com>
2021-03-21 14:13:29 +0600

    feat: add pick type

--- full-stack/frontend/react-app/react-typescript-app/src/utilities/mapped-type.tsx
+++ full-stack/frontend/react-app/react-typescript-app/src/utilities/mapped-type.tsx
@@ -1,4 +1,13 @@
+export type Properties = "propA" | "propB";
 type MyMappedType<T> = {
   [P in keyof T]: T[P];
 };
 type MyNewType = MyMappedType<{ a: 'a', b: 'b'}>
+
+const data = { a: 'a', b: 'b', boo: true}
+const hello: MyMappedType<typeof data> = data
+
+type Pick1<T, Properties extends keyof T> = {
+  [P in Properties]: T[P]
+}
+type MyNewType2 = Pick1<{a: 'a'; b: 'b'}, 'a' | 'b'>
