550bc1a93c5568e3cde057a75ee5d6a055c56a64
mp5maker <khan.photon@gmail.com>
2021-03-22 14:40:40 +0600

    feat: added options api

--- full-stack/frontend/vue-app/simeple-router-app/src/views/jobs/Jobs.vue
+++ full-stack/frontend/vue-app/simeple-router-app/src/views/jobs/Jobs.vue
@@ -1,11 +1,14 @@
 <template>
   <div>
     <h1>Jobs</h1>
-    <div v-for="job in jobs" :key="job.id">
-      <router-link :to="{ name: 'JobDetails', params: { id: job.id } }">
-        <h2>{{ job.title }}</h2>
-      </router-link>
+    <div v-if="jobs.length">
+      <div v-for="job in jobs" :key="job.id">
+        <router-link :to="{ name: 'JobDetails', params: { id: job.id } }">
+          <h2>{{ job.title }}</h2>
+        </router-link>
+      </div>
     </div>
+    <div v-else>Loading...</div>
   </div>
 </template>
 
