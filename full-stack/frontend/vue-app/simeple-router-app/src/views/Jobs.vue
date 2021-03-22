baf54ff898263d180d6ead1595870e7ddd927ec9
mp5maker <khan.photon@gmail.com>
2021-03-22 13:38:30 +0600

    feat: add simple routing system with the vue app

---
+++ full-stack/frontend/vue-app/simeple-router-app/src/views/Jobs.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h1>Jobs</h1>
+    <div v-for="job in jobs" :key="job.id">
+      {{ job.title }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      jobs: [
+        { title: "UX Designer", id: 1, detail: "lorem" },
+        { title: "Web Developer", id: 2, detail: "lorem" },
+        { title: "View Developer", id: 3, detail: "lorem" },
+      ],
+    };
+  },
+};
+</script>
