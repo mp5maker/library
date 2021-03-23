761d13eaf1927cd9fb08381d76622515f6361994
mp5maker <khan.photon@gmail.com>
2021-03-23 14:34:16 +0600

    feat: add transition hooks

--- full-stack/frontend/vue-app/vue-animations/src/components/Todos.vue
+++ full-stack/frontend/vue-app/vue-animations/src/components/Todos.vue
@@ -6,14 +6,16 @@
       @keypress.enter="addTodo"
       placeholder="Add a new todo..."
     />
-    <div v-if="todos.length">
-      <transition-group tag="ul" name="list" appear>
-        <li v-for="todo in todos" :key="todo.id" @click="deleteTodo(todo.id)">
-          {{ todo.text }}
-        </li>
-      </transition-group>
-    </div>
-    <div v-else>Woohoo, nothing left todo!</div>
+    <transition name="switch" mode="out-in">
+      <div v-if="todos.length">
+        <transition-group tag="ul" name="list" appear>
+          <li v-for="todo in todos" :key="todo.id" @click="deleteTodo(todo.id)">
+            {{ todo.text }}
+          </li>
+        </transition-group>
+      </div>
+      <div v-else>Woohoo, nothing left todo!</div>
+    </transition>
   </div>
 </template>
 
@@ -102,5 +104,22 @@ input {
 }
 .list-move {
   transition: all 0.3s ease;
+}
+/* Switch Transitions */
+.switch-enter-from,
+.switch-leave-to {
+  opacity: 0;
+  transform: translateY(20px);
+}
+.switch-enter-to,
+.switch-leave-from {
+  opacity: 1;
+  transform: translateY(0);
+}
+.switch-enter-active {
+  transition: 0.5s all ease;
+}
+.switch-leave-active {
+  transition: all 0.5s ease;
 }
 </style>
--- full-stack/frontend/vue-app/vue-animations/src/views/About.vue
+++ full-stack/frontend/vue-app/vue-animations/src/views/About.vue
@@ -1,24 +1,89 @@
 <template>
   <div class="about">
-    <h1>About</h1>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum aperiam officia possimus delectus inventore quod quisquam culpa voluptas iusto, quae maiores quo dolorum, corporis laboriosam a dolore consequatur assumenda nam!</p>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum aperiam officia possimus delectus inventore quod quisquam culpa voluptas iusto, quae maiores quo dolorum, corporis laboriosam a dolore consequatur assumenda nam!</p>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum aperiam officia possimus delectus inventore quod quisquam culpa voluptas iusto, quae maiores quo dolorum, corporis laboriosam a dolore consequatur assumenda nam!</p>
+    <transition
+      name="fade"
+      @before-enter="beforeEnter"
+      @enter="enter"
+      @after-enter="afterEnter"
+      @before-leave="beforeLeave"
+      @leave="leave"
+      @after-leave="afterLeave"
+      appear
+    >
+      <h1 v-if="showTitle">About</h1>
+    </transition>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum aperiam
+      officia possimus delectus inventore quod quisquam culpa voluptas iusto,
+      quae maiores quo dolorum, corporis laboriosam a dolore consequatur
+      assumenda nam!
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum aperiam
+      officia possimus delectus inventore quod quisquam culpa voluptas iusto,
+      quae maiores quo dolorum, corporis laboriosam a dolore consequatur
+      assumenda nam!
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum aperiam
+      officia possimus delectus inventore quod quisquam culpa voluptas iusto,
+      quae maiores quo dolorum, corporis laboriosam a dolore consequatur
+      assumenda nam!
+    </p>
   </div>
 </template>
 
 <script>
-
+import { ref } from "vue";
 export default {
   setup() {
+    const showTitle = ref(true);
+    const beforeEnter = (el) => {
+      console.log(el);
+    };
+    const enter = (el) => {
+      console.log(el);
+    };
+    const afterEnter = (el) => {
+      console.log(el);
+      setTimeout(() => (showTitle.value = false), 2000);
+    };
 
-  }
-}
+    const beforeLeave = (el) => {
+      console.log(el);
+    };
+    const leave = (el) => {
+      console.log(el);
+    };
+    const afterLeave = (el) => {
+      console.log(el);
+    };
+
+    return {
+      enter,
+      beforeEnter,
+      afterEnter,
+      showTitle,
+      beforeLeave,
+      leave,
+      afterLeave,
+    };
+  },
+};
 </script>
 
 <style>
-  .about {
-    max-width: 600px;
-    margin: 20px auto;
-  }
+.about {
+  max-width: 600px;
+  margin: 20px auto;
+}
+.fade-enter-from {
+  opacity: 0;
+}
+.fade-enter-active {
+  transition: opacity 3s ease;
+}
+.fade-leave-active {
+  transition: opacity 3s ease;
+}
 </style>
