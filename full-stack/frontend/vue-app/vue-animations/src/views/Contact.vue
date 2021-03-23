4c1c0fb40a430d40b001782fce8284d5a57150cd
mp5maker <khan.photon@gmail.com>
2021-03-23 14:49:06 +0600

    feat: added staggered effect in vue using gsap

--- full-stack/frontend/vue-app/vue-animations/src/views/Contact.vue
+++ full-stack/frontend/vue-app/vue-animations/src/views/Contact.vue
@@ -1,48 +1,69 @@
 <template>
   <div class="contact">
     <h1>Contact</h1>
-    <ul>
-      <li v-for="icon in icons" :key="icon.name">
+    <transition-group
+      appear
+      tag="ul"
+      @before-enter="beforeEnter"
+      @enter="enter"
+    >
+      <li v-for="(icon, index) in icons" :key="icon.name" :data-index="index">
         <span class="material-icons">{{ icon.name }}</span>
         <div>{{ icon.text }}</div>
       </li>
-    </ul>
+    </transition-group>
   </div>
 </template>
 
 <script>
-import { ref } from 'vue'
+import { ref } from "vue";
+import gsap from "gsap";
 
 export default {
   setup() {
     const icons = ref([
-      { name: 'alternate_email', text: 'by email'},
-      { name: 'local_phone', text: 'by phone'},
-      { name: 'local_post_office', text: 'by post'},
-      { name: 'local_fire_department', text: 'by smoke signal'},
-    ])
+      { name: "alternate_email", text: "by email" },
+      { name: "local_phone", text: "by phone" },
+      { name: "local_post_office", text: "by post" },
+      { name: "local_fire_department", text: "by smoke signal" },
+    ]);
 
-    return { icons }
-  }
-}
+    const beforeEnter = (el, done) => {
+      el.style.opacity = 0;
+      el.style.transform = "translateY(100px)";
+    };
+
+    const enter = (el, done) => {
+      gsap.to(el, {
+        opacity: 1,
+        y: 0,
+        duration: 0.8,
+        onComplete: done,
+        delay: el.dataset.index * 0.2,
+      });
+    };
+
+    return { icons, beforeEnter, enter };
+  },
+};
 </script>
 
 <style>
-  .contact ul {
-    padding: 0;
-    display: grid;
-    grid-template-columns: 1fr 1fr;
-    grid-gap: 20px;
-    max-width: 400px;
-    margin: 60px auto;
-  }
-  .contact li {
-    list-style-type: none;
-    background: white;
-    padding: 30px;
-    border-radius: 10px;
-    box-shadow: 1px 3px 5px rgba(0,0,0,0.1);
-    cursor: pointer;
-    line-height: 1.5em;
-  }
+.contact ul {
+  padding: 0;
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-gap: 20px;
+  max-width: 400px;
+  margin: 60px auto;
+}
+.contact li {
+  list-style-type: none;
+  background: white;
+  padding: 30px;
+  border-radius: 10px;
+  box-shadow: 1px 3px 5px rgba(0, 0, 0, 0.1);
+  cursor: pointer;
+  line-height: 1.5em;
+}
 </style>
