f4d09a8ec80e1cb7bea9d9f0578004a1d1e51a49
mp5maker <khan.photon@gmail.com>
2021-03-23 09:56:17 +0600

    feat: add computed inside the composition api

--- full-stack/frontend/vue-app/blog-app/src/views/Home.vue
+++ full-stack/frontend/vue-app/blog-app/src/views/Home.vue
@@ -1,33 +1,78 @@
 <template>
   <div class="home">
     Home
-    <p ref="paragraphRef">My name is {{ name }} and my age is {{ age }}</p>
-    <button @click="handleClick">Click Me</button>
-    <button @click="age++">Add 1 to age</button>
-    <input type="text" v-model="name" />
+    <div>
+      <h2>Refs</h2>
+      <p ref="paragraphRef">
+        My name is {{ ninja.name }} and my age is {{ ninja.age }}
+      </p>
+      <button @click="updateNinja">Click Me</button>
+      <button @click="ninja.age++">Add 1 to age</button>
+      <input type="text" v-model="ninja.name" />
+    </div>
+    <div>
+      <h2>Reactive</h2>
+      <p>My name is {{ ninjaTwo.name }} and my age is {{ ninjaTwo.age }}</p>
+      <button @click="updateNinjaTwo">Click Me</button>
+    </div>
+    <div>
+      <input type="text" v-model="search" placeholder="search" />
+      <div v-for="name in matchingNames" :key="name">
+        {{ name }}
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import { ref } from "vue";
+import { ref, reactive, computed } from "vue";
 
 export default {
   name: "Home",
   setup() {
-    const paragraphRef = ref(null);
-
-    // These are not reactive values
     console.log("setup");
-    let name = ref("mario");
-    let age = ref(30);
+    const paragraphRef = ref(null);
+    const ninja = ref({ name: "mario", age: 30 });
+    // Reactive do not accepts primitive type
+    const ninjaTwo = reactive({ name: "luigi", age: 35 });
+    const names = ref([
+      "mario",
+      "yoshi",
+      "luigi",
+      "toad",
+      "bowser",
+      "koopa",
+      "peach",
+    ]);
+    const search = ref("");
 
-    const handleClick = () => {
-      name.value = "john";
-      age.value = 27;
+    const matchingNames = computed(() => {
+      return names.value.filter(
+        (name) => name.toLowerCase().indexOf(search.value.toLowerCase()) > -1
+      );
+    });
+
+    const updateNinja = () => {
+      ninja.value.name = "john";
+      ninja.value.age = 27;
       console.log(paragraphRef, paragraphRef.value);
     };
 
-    return { name, age, handleClick, paragraphRef };
+    const updateNinjaTwo = () => {
+      ninjaTwo.name = "mario";
+      ninjaTwo.age = 33;
+    };
+
+    return {
+      ninja,
+      updateNinja,
+      paragraphRef,
+      ninjaTwo,
+      updateNinjaTwo,
+      names,
+      search,
+      matchingNames,
+    };
   },
   created() {
     console.log("created");
