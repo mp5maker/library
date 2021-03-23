1552fd47e62dcdc2bb7faf5a8328f8acef0050a1
mp5maker <khan.photon@gmail.com>
2021-03-23 16:21:53 +0600

    feat: added a simple vue native app

---
+++ full-stack/frontend/vue-native-app/vue-basic/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <view class="container">
+    <button :on-press="changeThemeLight" title="LIGHT" />
+    <button :on-press="changeThemeDark" title="DARK" />
+  </view>
+</template>
+
+<script>
+import Settings from "./constants/settings.native";
+
+export default {
+  name: "App",
+  data() {
+    return {
+      theme: "LIGHT",
+    };
+  },
+  methods: {
+    changeThemeLight() {
+      this.theme = "LIGHT";
+      console.log("light");
+      this.$emit(Settings.EMIT.THEME_CHANGE, "LIGHT");
+    },
+    changeThemeDark() {
+      this.theme = "DARK";
+      console.log("dark");
+      this.$emit(Settings.EMIT.THEME_CHANGE, "DARK");
+    },
+  },
+  components: {},
+};
+</script>
+
+
+<style>
+.container {
+  background-color: white;
+  align-items: center;
+  justify-content: center;
+  flex: 1;
+}
+.text-color-primary {
+  color: blue;
+}
+</style>
---
+++ full-stack/frontend/vue-native-app/vue-basic/constants/settings.native.js
@@ -0,0 +1,9 @@
+export default {
+  THEME: {
+    LIGHT: 'LIGHT',
+    DARK: 'DARK'
+  },
+  EMIT: {
+    THEME_CHANGE: 'theme-change'
+  }
+}
