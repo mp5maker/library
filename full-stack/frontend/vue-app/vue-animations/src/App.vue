640c453c278af43b52ab24e6494a41c863f5952a
mp5maker <khan.photon@gmail.com>
2021-03-23 14:55:13 +0600

    feat: added transition to the vue routes

--- full-stack/frontend/vue-app/vue-animations/src/App.vue
+++ full-stack/frontend/vue-app/vue-animations/src/App.vue
@@ -1,10 +1,16 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link> | 
-    <router-link to="/contact">Contact</router-link>
+  <div>
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link> |
+      <router-link to="/contact">Contact</router-link>
+    </div>
+    <router-view v-slot="{ Component }">
+      <transition name="route" mode="out-in">
+        <component :is="Component"></component>
+      </transition>
+    </router-view>
   </div>
-  <router-view/>
 </template>
 
 <style>
@@ -24,5 +30,27 @@ body {
 }
 #nav a.router-link-exact-active {
   color: #42b983;
+}
+/* Route Transitions */
+.route-enter-from {
+  opacity: 0;
+  transform: translateX(100px);
+}
+.route-enter-to {
+  opacity: 1;
+  transform: translateX(0);
+}
+
+.route-enter-active {
+  transition: all 0.3s linear;
+}
+
+.route-leave-to {
+  opacity: 0;
+  transform: translateX(-100px);
+}
+
+.route-leave-active {
+  transition: all 0.3s ease-in;
 }
 </style>
