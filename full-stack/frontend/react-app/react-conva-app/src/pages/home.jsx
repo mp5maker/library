1c9229d2ad883dd1e857c5deee68b64dc3c24554
mp5maker <khan.photon@gmail.com>
2021-04-24 12:59:38 +0600

    feat: updated the conva functions

--- full-stack/frontend/react-app/react-conva-app/src/pages/home.jsx
+++ full-stack/frontend/react-app/react-conva-app/src/pages/home.jsx
@@ -28,54 +28,58 @@ const Home = () => {
 
   console.log(image);
 
-  return image ? (
-    <Stage width={window.innerWidth} height={window.innerHeight}>
-      <Layer>
-        <Image image={image} x={0} y={window.innerHeight / 2} draggable />
-        <Text text={"Try to drag the star"} />
-        <Star
-          key={1}
-          id={1}
-          x={window.innerWidth / 2}
-          y={window.innerHeight / 2}
-          numPoints={5}
-          innerRadius={20}
-          outerRadius={50}
-          fill={"#89b717"}
-          opacity={0.8}
-          draggable
-          rotation={0}
-          shadowColor={"black"}
-          shadowBlur={10}
-          shadowOpacity={0.6}
-          shadowOffsetX={5}
-          shadowOffsetY={5}
-          scaleX={isStarDragging ? 1.2 : 1}
-          scaleY={isStarDragging ? 1.2 : 1}
-          onDragStart={handleDragStart}
-          onDragEnd={handleDragEnd}
-        />
-        <Rect
-          width={50}
-          height={50}
-          x={window.innerWidth / 3}
-          y={window.innerHeight / 3}
-          fill="red"
-          draggable
-        />
-        <Circle
-          x={200}
-          y={200}
-          stroke="black"
-          radius={50}
-          draggable
-          x={window.innerWidth / 2}
-          y={window.innerHeight / 2}
-        />
-      </Layer>
-    </Stage>
-  ) : (
-    <></>
+  return (
+    <div>
+      {image ? (
+        <Stage width={window.innerWidth} height={window.innerHeight}>
+          <Layer>
+            <Image image={image} x={0} y={window.innerHeight / 2} draggable />
+            <Text text={"Try to drag the star"} />
+            <Star
+              key={1}
+              id={1}
+              x={window.innerWidth / 2}
+              y={window.innerHeight / 2}
+              numPoints={5}
+              innerRadius={20}
+              outerRadius={50}
+              fill={"#89b717"}
+              opacity={0.8}
+              draggable
+              rotation={0}
+              shadowColor={"black"}
+              shadowBlur={10}
+              shadowOpacity={0.6}
+              shadowOffsetX={5}
+              shadowOffsetY={5}
+              scaleX={isStarDragging ? 1.2 : 1}
+              scaleY={isStarDragging ? 1.2 : 1}
+              onDragStart={handleDragStart}
+              onDragEnd={handleDragEnd}
+            />
+            <Rect
+              width={50}
+              height={50}
+              x={window.innerWidth / 3}
+              y={window.innerHeight / 3}
+              fill="red"
+              draggable
+            />
+            <Circle
+              x={200}
+              y={200}
+              stroke="black"
+              radius={50}
+              draggable
+              x={window.innerWidth / 2}
+              y={window.innerHeight / 2}
+            />
+          </Layer>
+        </Stage>
+      ) : (
+        <></>
+      )}
+    </div>
   );
 };
 
