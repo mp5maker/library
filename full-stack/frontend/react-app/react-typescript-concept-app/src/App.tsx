eb6e1a79c39129ba80c2d4b5b5491709d31be2df
mp5maker <khan.photon@gmail.com>
2021-04-24 15:22:38 +0600

    feat: added a search function

--- full-stack/frontend/react-app/react-typescript-concept-app/src/App.tsx
+++ full-stack/frontend/react-app/react-typescript-concept-app/src/App.tsx
@@ -3,26 +3,42 @@ import "./App.css";
 import widgets from "./mock-data/widgets";
 import get from "lodash/get";
 import people from "./mock-data/people";
+import genericSearch from "./utilities/genericSearch";
+import { SearchInput } from "./components/SearchInput";
 
 function App() {
+  const [query, setSearchQuery] = React.useState<string>("");
+
   return (
     <>
       <div>
+        <SearchInput setSearchQuery={setSearchQuery} />
+      </div>
+
+      <div>
         <h2> Widgets</h2>
-        {widgets.map((widget) => {
-          const id = get(widget, "id", "");
-          return <p key={String(id)}>{widget.title}</p>;
-        })}
+        {widgets
+          .filter((widget) =>
+            genericSearch(widget, ["title", "description"], query)
+          )
+          .map((widget) => {
+            const id = get(widget, "id", "");
+            return <p key={String(id)}>{widget.title}</p>;
+          })}
       </div>
       <div>
         <h2> People</h2>
-        {people.map((person, index) => {
-          return (
-            <p key={String(index)}>
-              {person.firstName} {person.lastName}
-            </p>
-          );
-        })}
+        {people
+          .filter((widget) =>
+            genericSearch(widget, ["firstName", "lastName", "eyeColor"], query)
+          )
+          .map((person, index) => {
+            return (
+              <p key={String(index)}>
+                {person.firstName} {person.lastName}
+              </p>
+            );
+          })}
       </div>
     </>
   );
---
+++ full-stack/frontend/react-app/react-typescript-concept-app/src/utilities/genericSearch.ts
@@ -0,0 +1,21 @@
+import get from "lodash/get";
+
+export default function genericSearch<T>(
+  object: T,
+  properties: Array<keyof T>,
+  query: string,
+  shouldBeCaseSensitive: boolean = false
+): boolean {
+  if (query == "") return true;
+
+  return properties.some((property) => {
+    const value = get(object, property, "");
+    if (typeof value == "string" || typeof value == "number") {
+      if (shouldBeCaseSensitive) {
+        return value.toString().includes(query);
+      }
+      return value.toString().toLowerCase().includes(query.toLowerCase());
+    }
+    return false;
+  });
+}
