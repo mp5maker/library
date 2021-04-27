708474820f2bc3d0dc8779db20fa1986868a126c
mp5maker <khan.photon@gmail.com>
2021-04-27 23:34:30 +0600

    feat: demystifying parallax

---
+++ css/parallax/track-inputs/src/index.js
@@ -0,0 +1,5 @@
+import head from 'lodash/head'
+
+(function() {
+  console.log('hello')
+})();
---
+++ css/parallax/track-inputs/webpack.config.js
@@ -0,0 +1,34 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  devServer: {
+    hot: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: "index.html",
+      template: path.resolve(__dirname, "index.html"),
+    }),
+  ],
+};
