8df27ebf9984a893dffd78ec327a194157c6fc19
mp5maker <khan.photon@gmail.com>
2021-08-27 14:51:26 +0600

    feat: add fibonacci sequence in recursive programming

---
+++ data-structure-and-algorithm/topic-wise/dynamic-programming/fibonacci.js
@@ -0,0 +1,13 @@
+const fibonacci = (_n) => {
+  const recursive = (n, memo = []) => {
+    if (n == 0) return 0
+    if (n == 1) return 1
+    if (memo[n]) return memo[n]
+    memo[n] = recursive(n - 2, memo) + recursive(n - 2, memo)
+    return memo[n]
+  }
+
+  return recursive(_n)
+}
+
+console.log(fibonacci(5))
