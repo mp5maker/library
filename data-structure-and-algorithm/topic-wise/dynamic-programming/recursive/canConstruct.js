6da8818203db684a175ca253c04215bf063100b9
mp5maker <khan.photon@gmail.com>
2021-08-30 11:33:10 +0600

    feat: add memo for the can construct

--- data-structure-and-algorithm/topic-wise/dynamic-programming/recursive/canConstruct.js
+++ data-structure-and-algorithm/topic-wise/dynamic-programming/recursive/canConstruct.js
@@ -1,19 +1,21 @@
 const canConstruct = (_s, _arr) => {
-  const recursive = (s, arr) => {
-      if (s == '') return [[]]
+  const recursive = (s, arr, memo = {}) => {
+    if (memo[s]) return memo[s];
+    if (s == "") return [[]];
 
-      const result = []
+    const result = [];
 
-      for (let word of arr) {
-        if (s.indexOf(word) == 0) {
-          const remainingWord = s.slice(word.length)
-          const suffixWays = recursive(remainingWord, arr)
-          const targetWays = suffixWays.map((item) => [word, ...item])
-          result.push(...targetWays)
-        }
+    for (let word of arr) {
+      if (s.indexOf(word) == 0) {
+        const remainingWord = s.slice(word.length);
+        const suffixWays = recursive(remainingWord, arr, memo);
+        const targetWays = suffixWays.map((item) => [word, ...item]);
+        result.push(...targetWays);
       }
+    }
 
-      return result;
+    memo[s] = result;
+    return result;
   };
 
   return recursive(_s, _arr);
