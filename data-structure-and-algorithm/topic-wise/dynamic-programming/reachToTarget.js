818eb199a15bfc8fc494d5e39363cc06cbdf2ffe
mp5maker <khan.photon@gmail.com>
2021-08-30 09:38:11 +0600

    feat: reach to target have been modified

--- data-structure-and-algorithm/topic-wise/dynamic-programming/reachToTarget.js
+++ data-structure-and-algorithm/topic-wise/dynamic-programming/reachToTarget.js
@@ -18,3 +18,27 @@ const reachToTarget = (target, candidates) => {
 }
 
 console.log(reachToTarget(20, [3, 5, 10]))
+
+// const reachToTarget = (target, candidates, memo = {}) => {
+//   const recursive = (targetSum, candidates) => {
+//     if (targetSum == 0) return [[]];
+//     if (targetSum < 0) return null;
+//     if (memo[targetSum]) return memo[targetSum]
+
+//     let result = [];
+//     for (let candidate of candidates) {
+//       const remainder = targetSum - candidate;
+//       const remainderWays = recursive(remainder, candidates, memo);
+//       if (remainderWays) {
+//         const targetWays = remainderWays.map((ways) => [candidate, ...ways]);
+//         result.push(...targetWays);
+//       }
+//     }
+//     memo[targetSum] = result
+//     return result;
+//   };
+
+//   return recursive(target, candidates);
+// };
+
+// console.log(reachToTarget(20, [3, 5, 10]));
