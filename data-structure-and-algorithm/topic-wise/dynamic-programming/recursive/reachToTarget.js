e718f7a88ce31813edbb0a9b98604d3207aa9783
mp5maker <khan.photon@gmail.com>
2021-09-23 10:49:43 +0600

    feat: add reach to target recursion

--- data-structure-and-algorithm/topic-wise/dynamic-programming/recursive/reachToTarget.js
+++ data-structure-and-algorithm/topic-wise/dynamic-programming/recursive/reachToTarget.js
@@ -1,44 +1,25 @@
-const reachToTarget = (target, candidates) => {
-  let res = []
-  const recursive = (i, combinations, total) => {
-    if (total == target) {
-      res.push([...combinations])
-      return
-    }
-    if (i >= candidates.length || total > target) return
+const reachToTarget = (_targetSum, _numbers) => {
+  const recursive = (targetSum, numbers, memo = {}) => {
+    if (targetSum == 0) return [[]]
+    if (targetSum < 0) return null
+    if (memo[targetSum]) return memo[targetSum]
 
-    const current = candidates[i];
-    combinations.push(current)
-    recursive(i, combinations, total + current)
-    combinations.pop()
-    recursive(i + 1, combinations, total)
+    let result = []
+    for (let i = 0; i < numbers.length; i++) {
+      const current = numbers[i]
+      const destinations = recursive(targetSum - current, numbers, memo)
+      if (destinations) {
+        const targetWays = destinations.map((ways) => [current, ...ways])
+        result.push(...targetWays)
+      }
+    }
+
+    memo[targetSum] = result
+    return result
   }
-  recursive(0, [], 0)
-  return res
+
+  return recursive(_targetSum, _numbers)
 }
 
-console.log(reachToTarget(20, [3, 5, 10]))
 
-// const reachToTarget = (target, candidates, memo = {}) => {
-//   const recursive = (targetSum, candidates) => {
-//     if (targetSum == 0) return [[]];
-//     if (targetSum < 0) return null;
-//     if (memo[targetSum]) return memo[targetSum]
-
-//     let result = [];
-//     for (let candidate of candidates) {
-//       const remainder = targetSum - candidate;
-//       const remainderWays = recursive(remainder, candidates, memo);
-//       if (remainderWays) {
-//         const targetWays = remainderWays.map((ways) => [candidate, ...ways]);
-//         result.push(...targetWays);
-//       }
-//     }
-//     memo[targetSum] = result
-//     return result;
-//   };
-
-//   return recursive(target, candidates);
-// };
-
-// console.log(reachToTarget(20, [3, 5, 10]));
+console.log(reachToTarget(20, [3, 5, 10]));
