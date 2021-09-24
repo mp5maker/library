51eac52a88944d7317d5c80bba1359f2a7d43d2c
mp5maker <khan.photon@gmail.com>
2021-09-24 11:52:19 +0600

    feat: add another dp problem (house robber)

---
+++ data-structure-and-algorithm/topic-wise/dynamic-programming/tabulation/houseRobber2.js
@@ -0,0 +1,20 @@
+const houseRobber2 = (nums) => {
+  const performAction = (numbers) => {
+    let table = Array.from({ length: numbers.length });
+    table[0] = numbers[0];
+    table[1] = Math.max(numbers[0], numbers[1]);
+
+    for (let i = 2; i < numbers.length; i++) {
+      table[i] = Math.max(numbers[i] + table[i - 2], table[i - 1]);
+    }
+
+    return table[numbers.length - 1];
+  };
+  return Math.max(
+    nums[0],
+    performAction(nums.slice(1)),
+    performAction(nums.slice(0, nums.length - 1))
+  );
+};
+
+console.log(houseRobber2([1, 2, 3, 1]));
