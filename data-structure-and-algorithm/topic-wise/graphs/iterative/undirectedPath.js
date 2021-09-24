80ff1ae8388d701e1b7703d96c606ae8c569f911
mp5maker <khan.photon@gmail.com>
2021-09-24 21:30:20 +0600

    feat: add undirected path

--- data-structure-and-algorithm/topic-wise/graphs/iterative/undirectedPath.js
+++ data-structure-and-algorithm/topic-wise/graphs/iterative/undirectedPath.js
@@ -1,31 +1,35 @@
-const buildGraph = (edges) => {
+const convertToGraph = (edges) => {
   let graph = {};
-  for (let edge of edges) {
-    const [a, b] = edge;
-    if (!graph[a]) graph[a] = [];
-    if (!graph[b]) graph[b] = [];
-    graph[a].push(b);
-    graph[b].push(a);
+  for (let i = 0; i < edges.length; i++) {
+    const [first, second] = edges[i];
+    graph[first] = [...(graph[first] ? [...graph[first], second] : [second])];
+    graph[second] = [...(graph[second] ? [...graph[second], first] : [first])];
   }
   return graph;
 };
 
-const hasPath = (graph, src, dst, visited) => {
-  if (visited.has(src)) return false;
-  visited.add(src)
-  if (src == dst) return true;
-  for (let neighbor of graph[src]) {
-    if (hasPath(graph, neighbor, dst, visited)) {
-      return true;
+const hasPath = (graph, src, dest) => {
+  const stack = [src];
+  const visited = [];
+
+  while (stack.length) {
+    const current = stack.pop();
+    if (!visited.includes(current)) {
+      visited.push(current);
+      if (current == dest) return true;
+      const neighbors = graph[current];
+      for (let neighbor of neighbors) {
+        stack.push(neighbor);
+      }
     }
   }
+
   return false;
 };
 
-const undirectedPath = (edges, nodeA, nodeB) => {
-  const graph = buildGraph(edges);
-  const hasPath = (graph, nodeA, nodeB, new Set());
-  return hasPath;
+const undirectedPath = (edges, src, dest) => {
+  const graph = convertToGraph(edges);
+  return hasPath(graph, src, dest);
 };
 
 const edges = [
@@ -36,4 +40,4 @@ const edges = [
   ["o", "n"],
 ];
 
-undirectedPath(edges, "j", "m");
+console.log(undirectedPath(edges, "j", "m"));
