8e0f9c731b2698f26a08a0dd38067fce7d1f20d7
mp5maker <khan.photon@gmail.com>
2021-10-03 20:07:43 +0600

    fix: reach to target dynaming programming

--- data-structure-and-algorithm/topic-wise/dynamic-programming/tabulation/reachToTarget.js
+++ data-structure-and-algorithm/topic-wise/dynamic-programming/tabulation/reachToTarget.js
@@ -1,15 +1,12 @@
 const reachToTarget = (targetSum, numbers) => {
-  const table = Array.from(({ length: targetSum + 1 })).fill(0)
-  table[0] = 1
-  for (let i = 0; i <= targetSum; i++) {
-    for (let k = 0; k < numbers.length; k++) {
-      const number = numbers[k]
-      if (i + number <= targetSum) {
-        table[i + number] += table[i]
-      }
+  let table = Array.from({ length: targetSum + 1 }, () => 0);
+  table[0] = 1;
+  for (let num of numbers) {
+    for (let i = num; i <= targetSum; i++) {
+      table[i] += table[i - num];
     }
   }
-  return table[targetSum]
-}
+  return table[targetSum];
+};
 
 console.log(reachToTarget(20, [3, 5, 10]));
