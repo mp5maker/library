585d54955a7a042f009c6299c93284536cd96c27
mp5maker <khan.photon@gmail.com>
2021-10-07 09:17:26 +0600

    feat: updated the code for permuation

--- data-structure-and-algorithm/topic-wise/dynamic-programming/tabulation/permutation.js
+++ data-structure-and-algorithm/topic-wise/dynamic-programming/tabulation/permutation.js
@@ -1,10 +1,9 @@
-let arr = []
-const permutation = (str, prefix = "") => {
+const permutation = (str, prefix = "", arr = []) => {
   if (str.length == 0) return arr.push(prefix)
   for (let i = 0; i < str.length; i++) {
     const char = str[i]
     const newStr = str.substring(0, i) + str.substring(i + 1)
-    permutation(newStr, prefix + char)
+    permutation(newStr, prefix + char, arr)
   }
   return arr
 };
