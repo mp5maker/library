adf3675b63c5435f016478fb5e039b4f6d9e5ae6
mp5maker <khan.photon@gmail.com>
2021-10-07 19:25:17 +0600

    feat: add print and contains method in linked list

--- data-structure-and-algorithm/algorithm-club/linked-list/linked-list.js
+++ data-structure-and-algorithm/algorithm-club/linked-list/linked-list.js
@@ -34,6 +34,27 @@ class LinkedList {
     this.head.next = current
     return this.head
   }
+
+  print() {
+    let current = this.head
+    let arr = []
+    while(current) {
+      arr.push(current.val)
+      current = current.next
+    }
+    return arr.join('->')
+  }
+
+  contains(val) {
+    let current = this.head
+
+    while(current) {
+      if (current.val == val) return true
+      current = current.next
+    }
+
+    return false
+  }
 }
 
 const ll = new LinkedList()
@@ -42,4 +63,6 @@ ll.append(5)
 ll.append(11)
 ll.append(12)
 ll.prepend(100)
-console.log(ll.head)
+console.log(ll.print())
+console.log(ll.contains(20))
+console.log(ll.contains(11))
