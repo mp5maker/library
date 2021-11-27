14f6c4ae2ec8d24d4a950199f14f4d2e184abaa1
mp5maker <khan.photon@gmail.com>
2021-11-27 22:22:44 +0600

    feat: updated the client app

--- microservices/blog/client/src/PostList.js
+++ microservices/blog/client/src/PostList.js
@@ -7,7 +7,7 @@ const PostList = () => {
   const [posts, setPosts] = useState({});
 
   const fetchPosts = async () => {
-    const res = await axios.get("http://localhost:4000/posts");
+    const res = await axios.get("http://localhost:4002/posts");
 
     setPosts(res.data);
   };
@@ -17,6 +17,7 @@ const PostList = () => {
   }, []);
 
   const renderedPosts = Object.values(posts).map((post) => {
+    const { comments } = post
     return (
       <div
         className="card"
@@ -25,7 +26,7 @@ const PostList = () => {
       >
         <div className="card-body">
           <h3>{post.title}</h3>
-          <CommentList postId={post.id} />
+          <CommentList comments={comments} />
           <CommentCreate postId={post.id} />
         </div>
       </div>
