4962c302e5bc54c4f2838ed62e4c9953f227af84
mp5maker <khan.photon@gmail.com>
2022-12-28 16:35:50 +0600

    :sparkles: feat: added the chrome actions, storage and alarm api

--- chrome-extensions/timer-extension/popup.js
+++ chrome-extensions/timer-extension/popup.js
@@ -1,11 +1,16 @@
-const timeElement = document.getElementById('time')
-const nameElement = document.getElementById("name")
-const currentTime = new Date().toLocaleTimeString()
-timeElement.textContent = `The time is: ${currentTime}`
 
-/**
- * Getting Storage
- */
+const nameElement = document.getElementById("name")
 chrome.storage.sync.get(["name"], (response) => {
   nameElement.textContent = response.name ? `Your name is: ${response.name}` : ''
 });
+
+const timerElement = document.getElementById("timer")
+chrome.storage.local.get(["timer"], (response) => {
+  const time = response.timer ?? 0
+  timerElement.textContent = `The timer is at: ${time} seconds`
+})
+
+const timeElement = document.getElementById('time')
+timeElement.textContent = `The time is: ${currentTime}`
+const currentTime = new Date().toLocaleTimeString()
+timeElement.textContent = `The time is: ${currentTime}`
